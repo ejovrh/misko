3cb8366c248790658f40c920039a2c7b7add3248
ejovrh <hrvoje.ivanic@gmx.net>
2016-07-24 20:17:11 +0200

    - provided descriptions to selected pins

--- misko-prototype/pin_definitions.h
+++ misko-prototype/pin_definitions.h
@@ -1,8 +1,8 @@
 //#define interrupt_0_pin 2 // adxl345 int 1
 
-#define gps_power_pin 4 // Use pin 4 to control power to the GPS
-#define gps_green_led_pin 5 // blinks on write onto SD card
-#define gps_red_led_pin 6 // blinks on lock
+#define gps_power_pin 4 // supplies power via PWM (100% duty cycle) to em406a
+#define gps_green_led_pin 5 // LED1, blinks on write onto SD card
+#define gps_red_led_pin 6 // LED2, on - GPS has fix, off - GPS has no fix
 
 //#define bluetooth_mosfet_gate 7 // goes to gate of mosfet, controlling power to bluetooth transmitter
 //#define bluetooth_activate_button 52 // button to power up the transmitter
@@ -12,12 +12,12 @@
 
 // UART Serial 3
 //free
+
 #define sd_ss_pin 10 // SD/SPI slave select
 #define sd_sck_pin 11 // SD/SPI serial clock
 #define sd_miso_pin 12 // SD/SPI master in slave out
 #define sd_mosi_pin 13 // SD/SPI master out slave in
-//#define battery_pin 14 // battery voltage
+//#define battery_pin 14 // battery voltage   
  
 //#define not_connected_yet 42 
 //#define temperature_pin 44 // temperature sensor
-
