05ff04dba866e110c0de0465de04d8a3d2f093a1
ejovrh <hrvoje.ivanic@gmx.net>
2016-07-25 19:37:52 +0200

    renamed led_mosfet_gate_pin to lcd_mosfet_gate_pin since it is a LCD connected to it, not a LED

--- misko-prototype/pin_modes.h
+++ misko-prototype/pin_modes.h
@@ -5,7 +5,7 @@ pinMode(bluetooth_mosfet_gate_pin, OUTPUT); // bluetooth power control, bluetoot
 pinMode(gps_green_led_pin, OUTPUT); // GPS fix indiator: on - has fix, off - has no fix
 pinMode(gps_power_pin, OUTPUT); // powers the gps (PWM) -- FIXME: direct power through the atmega2560; better is using a mosfet, draws ~4mA regardless of fix
 pinMode(gps_red_led_pin, OUTPUT); // write cycle to SD card indicator (blinks on write), on by default until GPS gets fix, then off
-pinMode(led_mosfet_gate_pin, OUTPUT); // led module power control, FIXME: how much current??
+pinMode(lcd_mosfet_gate_pin, OUTPUT); // LCD module power control, FIXME: how much current??
 pinMode(sd_ss_pin, OUTPUT); // FIXME - add description
 
 // PIN STATES
@@ -13,5 +13,5 @@ digitalWrite(bluetooth_mosfet_gate_pin, HIGH); // mosfet gate for bluetooth modu
 digitalWrite(gps_green_led_pin, LOW); // off(low) by befault until GPS gets fix
 digitalWrite(gps_power_pin, LOW); // low(on), high(off) 
 digitalWrite(gps_red_led_pin, HIGH); // on(high) by default until gps gets fix, then off, blinks on SD card write
-digitalWrite(led_mosfet_gate_pin, HIGH); // on(high), low(off)
+digitalWrite(lcd_mosfet_gate_pin, HIGH); // on(high), low(off)
 
