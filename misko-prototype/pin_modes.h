f136becb03ad787f08c5b764bc9b8aa5f60b161f
ejovrh <hrvoje.ivanic@gmx.net>
2016-07-26 12:11:21 +0200

    added and defined state for bluetooth_power_button_pin

--- misko-prototype/pin_modes.h
+++ misko-prototype/pin_modes.h
@@ -2,6 +2,7 @@
 
 // PIN MODES
 pinMode(bluetooth_mosfet_gate_pin, OUTPUT); // bluetooth power control, bluetooth module draws ~40mA when not connected, 20-30 in connected state
+pinMode(bluetooth_power_button_pin, INPUT);
 pinMode(gps_green_led_pin, OUTPUT); // GPS fix indiator: on - has fix, off - has no fix
 pinMode(gps_power_pin, OUTPUT); // powers the gps (PWM) -- FIXME: direct power through the atmega2560; better is using a mosfet, draws ~4mA regardless of fix
 pinMode(gps_red_led_pin, OUTPUT); // write cycle to SD card indicator (blinks on write), on by default until GPS gets fix, then off
@@ -9,6 +10,7 @@ pinMode(lcd_mosfet_gate_pin, OUTPUT); // LCD module power control, draws 25mA (b
 pinMode(sd_ss_pin, OUTPUT); // FIXME - add description
 
 // PIN STATES
+digitalWrite(bluetooth_power_button_pin, HIGH);
 digitalWrite(bluetooth_mosfet_gate_pin, HIGH); // mosfet gate for bluetooth module, on(high), off(low)
 digitalWrite(gps_green_led_pin, LOW); // off(low) by befault until GPS gets fix
 digitalWrite(gps_power_pin, LOW); // low(on), high(off) 
