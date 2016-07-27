9412adaa013130fb4b55719bd10182b9af250207
ejovrh <hrvoje.ivanic@gmx.net>
2016-07-27 20:41:20 +0200

    - removed #include <EEPROMex.h> - added #include <EEPROM.h> instead - added char debug_out[80] for better debug output formatting - implemented "poor mans debugging" - a button press will print out items of interest

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -1,13 +1,16 @@
 // libraries
 #include <SoftwareSerial.h> // serial library
 #include <SD.h> // sd card library
-#include <EEPROMex.h> // extended EEPROM library
+#include <EEPROM.h> // EEPROM library
 
 // local files
 #include "gps_config.h" // gps stuff
 #include "pin_definitions.h" // pin layout
 
-#define CONFIG_VERSION 0 // version of the config structure in setup.h, if changes need to be written into EEPROM, it MUST be incremented
+//   CONFIG_VERSION MUST BE CHANGED IF ANY CHANGES ARE MADE IN setup.h
+#define CONFIG_VERSION 0 // protection against excessive EEPROM writes
+//   CONFIG_VERSION MUST BE CHANGED IF ANY CHANGES ARE MADE IN setup.h
+
 #define GPSRATE 4800
 #define SERIALRATE 9600
 //BLUETOOTHSERIALRATE is hardcoded in device
@@ -20,6 +23,8 @@ unsigned int bufferid = 0; // holds the current position in the NMEA_buffer arra
 char gps_date[9] = "20XXXXXX"; // 0-7 + 1 for '\0'
 char gps_time[7] = "XXXXXX"; // 0-5 + 1 for '\0'
 char gps_logfile[13] = "";
+
+char debug_out[80] = "";
 
 void setup() 
 {
@@ -49,8 +54,22 @@ void setup()
 } // setup end 
 
 #include "gps_functions.h" // gps functions
-
+   int printed = 0;
 void loop() 
 {
   get_nmea_sentences();
+
+  // poor mans debugging
+
+  if (digitalRead(bluetooth_power_button_pin) == HIGH && printed == 0)
+  {
+    Serial.println(" eeprom debug output: ");
+    for (int i = 0; i < 13; i++)
+    {
+      sprintf(debug_out, "%d - %d \n", i, EEPROM[0]);
+      Serial.print(debug_out);
+    }
+    printed = 1;
+  }
+  
 } 
