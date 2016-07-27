b4d49cb8eb22ea6b2a2a699a8271f24c34ba981f
ejovrh <hrvoje.ivanic@gmx.net>
2016-07-27 15:37:55 +0200

    -corrected comments

--- misko-prototype/setup.h
+++ misko-prototype/setup.h
@@ -7,7 +7,7 @@ typedef struct setup
   int lcd_auto_timeout = 2; // auto timeout in seconds
   int bluetooth_timeout = 60; // bluetooth timeout in seconds
   int timezone = 2; // UTC timezone
-  // byte order: leftmost: 0, rightmost 7; value 0(off), value 1(on)
+  // gps_settings byte order: leftmost: 0, rightmost 7; value 0(off), value 1(on)
   //  bit assignment
   //    0 - reserved - do not use
   //    1 - LOG_RMC - default on
@@ -17,7 +17,6 @@ typedef struct setup
   //    5 - LOG_GSV - default on
   //    6 - LOG_VTG - default off
   //    7 - USE_WAAS - default on
-        
   byte gps_settings = B01001101;
 } Setup;
 
