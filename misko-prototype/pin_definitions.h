913257caa0a2dbd36ea39b157c9025c7a755df9f
ejovrh <hrvoje.ivanic@gmx.net>
2016-07-30 15:44:42 +0200

    -added user button definitions

--- misko-prototype/pin_definitions.h
+++ misko-prototype/pin_definitions.h
@@ -3,6 +3,9 @@
 #define gps_green_led_pin 5 // LED1, blinks on write onto SD card
 #define gps_red_led_pin 6 // LED2, on - GPS has fix, off - GPS has no fix
 
+// UART Serial 1
+// used for bluetooth and GPS modules
+
 // UART Serial 2
 //free
 
@@ -14,7 +17,11 @@
 #define sd_miso_pin 12 // SD/SPI master in slave out
 #define sd_mosi_pin 13 // SD/SPI master out slave in
 
-#define bluetooth_power_button_pin 40
+#define menu_left_buttton 30 // user menu - left button, the buttons draw each 0.5 mA (we have 5 of them)
+#define menu_down_buttton 31 // user menu - down button
+#define menu_up_buttton 32 // user menu - up button
+#define menu_right_buttton 33 // user menu - right button
+#define bluetooth_power_toggle_pin 40 // user menu - bluetooth power toggle
 #define bluetooth_mosfet_gate_pin 48 // bluetooth power control via mosfet
 #define lcd_mosfet_gate_pin 49 // LCD power control via mosfet
 #define spi_miso_pin 50 // SPI - Master In Slave Out - not used yet
