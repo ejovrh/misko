0d6d844f31085d2fb3df197396e9453764c00fed
ejovrh <hrvoje.ivanic@gmx.net>
2016-07-31 15:37:07 +0200

    - moved #include "functions.h" further down - moved the bluetooth code into handle_bluetooth_button() in functions.h
    - introduced extremely bad copy-code for LCD button handling in functions.h
    it is really really bad code

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -4,7 +4,6 @@
 #include <EEPROM.h> // EEPROM library
 
 // local files
-#include "functions.h" // useful functions
 #include "gps_config.h" // gps stuff
 #include "pin_definitions.h" // pin layout
 
@@ -31,7 +30,16 @@ bool flag_bluetooth_is_on = 0; // flag is BT device is powered on or off
 bool flag_bluetooth_power_toggle_pressed = 0; // flag marks bluetooth button pressed or not - used to recognize button state change for proper high/low handling
 bool flag_bluetooth_power_keep_on = 0;
 
+unsigned long lcd_button_press_time = millis(); // time of button press
+bool flag_lcd_is_on = 0; // flag is BT device is powered on or off
+bool flag_lcd_button_down_pressed = 0; // flag marks button pressed or not
+bool flag_lcd_button_up_pressed = 0; // flag marks button pressed or not
+bool flag_lcd_button_left_pressed = 0; // flag marks button pressed or not
+bool flag_lcd_button_right_pressed = 0; // flag marks button pressed or not
+
 char debug_out[80] = "";
+
+#include "functions.h" // useful functions
 
 void setup() 
 {
@@ -64,58 +72,11 @@ void setup()
    int printed = 0;
 void loop() 
 {
-  if (digitalRead(menu_left_buttton) == HIGH)
-    Serial.println("left button press");
-
-  if (digitalRead(menu_right_buttton) == HIGH)
-    Serial.println("right button press");
-
-  if (digitalRead(menu_up_buttton) == HIGH)
-    Serial.println("up button press");
-
-  if (digitalRead(menu_down_buttton) == HIGH)
-    Serial.println("down button press");
-
-
-  // bluetooth power on
-  if (!flag_bluetooth_power_toggle_pressed && digitalRead(bluetooth_power_toggle_pin) == HIGH) // if button was not pressed and now gets pressed
-  //  flag_bluetooth_power_toggle_pressed prevents mutiple executions of this if() block
-  { 
-    bluetooth_button_press_time = millis(); // record time of button press; this is used in eeprom_timer()
-    digitalWrite(bluetooth_mosfet_gate_pin, HIGH); // turn on the device
-    flag_bluetooth_is_on = 1; // set flag to on
-    flag_bluetooth_power_toggle_pressed = 1; // mark button as pressed
-  }
-
-  // bluetooth power off
-  if (flag_bluetooth_power_toggle_pressed && digitalRead(bluetooth_power_toggle_pin) == LOW) // if button was  pressed and now gets released
-  // flag_bluetooth_power_toggle_pressed prevents mutiple executions of this if() block
-  { 
-    bluetooth_button_release_time = millis(); // record time of button press; this is used in 
-    flag_bluetooth_power_toggle_pressed = 0; // mark button as released
-
-    if (abs(bluetooth_button_release_time - bluetooth_button_press_time) > 500) // if button is held long enough
-    {
-      if (flag_bluetooth_power_keep_on) // if the BT device was on
-        digitalWrite(bluetooth_mosfet_gate_pin, LOW); // turn off the BT device
-
-      flag_bluetooth_power_keep_on = !flag_bluetooth_power_keep_on; // invert the flag (on -> off or off -> on)
-    }
-  }
-
-  // bluetooth timed power off
-  if ( (!flag_bluetooth_power_keep_on && flag_bluetooth_is_on) && eeprom_timer(bluetooth_button_press_time, 4)) // if the device is on and enough time has passed
-  // flag_bluetooth_power_keep_on prevents the timer from kicking in if we want the BT device to stay on
-  // flag_bluetooth_is_on prevents code execution on every loop
-  { 
-      digitalWrite(bluetooth_mosfet_gate_pin, LOW); // turn off the device
-      flag_bluetooth_is_on = 0; // set flag to off
-  }
-
-
-
-
-  get_nmea_sentences();
+  handle_bluetooth_button(); // handles the bluetooth power button
+  
+  handle_lcd_buttons(); // handles the lcd buttons
+  
+  get_nmea_sentences(); // gets NMEA sentences out of the GPS and deals with them
 
         /*
           // poor mans debugging
