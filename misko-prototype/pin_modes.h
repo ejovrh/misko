8244d100239db414108be6d3232467d3c82a48ae
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-01 23:32:16 +0200

    - commented out commands for lcd_mosfet_gate_pin

--- misko-prototype/pin_modes.h
+++ misko-prototype/pin_modes.h
@@ -6,7 +6,7 @@ pinMode(bluetooth_power_toggle_pin, INPUT);
 pinMode(gps_green_led_pin, OUTPUT); // GPS fix indiator: on - has fix, off - has no fix
 pinMode(gps_power_pin, OUTPUT); // powers the gps (PWM) -- FIXME: direct power through the atmega2560; better is using a mosfet, draws ~4mA regardless of fix
 pinMode(gps_red_led_pin, OUTPUT); // write cycle to SD card indicator (blinks on write), on by default until GPS gets fix, then off
-pinMode(lcd_mosfet_gate_pin, OUTPUT); // LCD module power control, draws 25mA (blank screen), FIXME: how much current exactly (off, with characters displayed)
+//pinMode(lcd_mosfet_gate_pin, OUTPUT); // LCD module power control, draws 25mA (blank screen), FIXME: how much current exactly (off, with characters displayed)
 pinMode(menu_down_buttton, INPUT); // user buttons - down
 pinMode(menu_left_buttton, INPUT); // user buttons - left 
 pinMode(menu_right_buttton, INPUT); //  user buttons - right
@@ -20,7 +20,7 @@ digitalWrite(bluetooth_mosfet_gate_pin, LOW); // mosfet gate for bluetooth modul
 digitalWrite(gps_green_led_pin, LOW); // off(low) by befault until GPS gets fix
 digitalWrite(gps_power_pin, LOW); // low(on), high(off) 
 digitalWrite(gps_red_led_pin, HIGH); // on(high) by default until gps gets fix, then off, blinks on SD card write
-digitalWrite(lcd_mosfet_gate_pin, LOW); // on(high), low(off)
+//digitalWrite(lcd_mosfet_gate_pin, LOW); // on(high), low(off)
 digitalWrite(menu_down_buttton, LOW); // on(high), low(off)
 digitalWrite(menu_left_buttton, LOW); // on(high), low(off)
 digitalWrite(menu_right_buttton, LOW); // on(high), low(off)
