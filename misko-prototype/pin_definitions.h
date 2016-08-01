a7dd578d4b9fa092ceb15e3916f7562d029d5e95
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-01 23:32:09 +0200

    - commented out commands for lcd_mosfet_gate_pin

--- misko-prototype/pin_definitions.h
+++ misko-prototype/pin_definitions.h
@@ -25,7 +25,7 @@
 #define oled_scl_pin 46 // OLED SCL
 #define oled_sda_pin 47 // OLED SDA - MOSI
 #define bluetooth_mosfet_gate_pin 48 // bluetooth power control via mosfet
-#define lcd_mosfet_gate_pin 49 // LCD power control via mosfet
+//#define lcd_mosfet_gate_pin 49 // LCD power control via mosfet
 #define spi_miso_pin 50 // SPI - Master In Slave Out - not used yet
 #define spi_mosi_pin 51 // SPI - Master Out Slave In - not used yet
 #define spi_sck_pin 52 // SPI - Serial Clock - not used yet
