40d1c78a5dc69f6f1deae75d07b71772141987fd
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-05 14:34:51 +0200

    - moved items from loop() into loop.h

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -4,7 +4,7 @@
 #include <EEPROM.h> // EEPROM library
 #include <U8glib.h> // https://github.com/olikraus/u8glib/wiki/userreference
 #include "M2tk.h" // https://code.google.com/archive/p/m2tklib/
-#include "utility/m2ghu8g.h"
+#include "utility/m2ghu8g.h" // the u8g graphics handler
 
 // local files
 #include "gps_config.h" // gps stuff
@@ -15,29 +15,11 @@
 void setup() 
 {
   #include "setup.h"
-} // setup end 
+}
 
 #include "gps_functions.h" // gps functions
 
 void loop() 
 {
-     m2.checkKey();
-
-    if (m2.handleKey() != 0)
-    {
-      // picture loop - https://github.com/olikraus/u8glib/wiki/tpictureloop
-      OLED.firstPage();  // https://github.com/olikraus/u8glib/wiki/userreference#firstpage
-      do 
-      {
-        draw(); // defined in functions.h
-      }
-      while( OLED.nextPage() ); // https://github.com/olikraus/u8glib/wiki/userreference#nextpage
-    } 
-    
-  handle_bluetooth_button(); // handles the bluetooth power button
-  
-  handle_lcd_buttons(); // handles the lcd buttons
-  
-  get_nmea_sentences(); // gets NMEA sentences out of the GPS and deals with them
-
+  #include "loop.h"
 } 
