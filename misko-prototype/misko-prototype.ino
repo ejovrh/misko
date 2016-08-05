eb99bf03d36bab1fdfd656e14b77affd1547159f
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-05 14:15:02 +0200

    - modified the picture loop - moved all from setup() into setup.h

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -14,49 +14,26 @@
 
 void setup() 
 {
-
-#include "pin_modes.h"
-#include "setup.h"
-  
-  Serial.begin(SERIALRATE); // connect to the serial terminal
-  Serial.println("start");
-
-  // initialize GPS
-  Serial3.begin(GPSRATE);   // connect to the GPS at the desired rate
-  Serial3.print(SERIAL_SET); // set gps serial comm. parameter
-  Serial3.print(WAAS_ON); // will become obsolete via EEPROM and setup
-  Serial3.print(RMC_ON); // will become obsolete via EEPROM and setup
-  Serial3.print(GSA_ON); // will become obsolete via EEPROM and setup
-  Serial3.print(GSV_ON); // will become obsolete via EEPROM and setup
-    
-  delay(50);
-  
-/*
-FIXME: seems to crash the graphics library
-  // initialize SD card  
-  if (!SD.begin(sd_ss_pin)) // see if the card is present and can be initialized
-  {  
-    Serial.println("SD Card failed, or not present");
-    return;
-  }  
-*/
-  m2_SetU8g(OLED.getU8g(), m2_u8g_box_icon); // connect u8glib with m2tklib
-  m2.setFont(0, u8g_font_6x10); // assign u8g_font_6x10 font to index 0
-  
+  #include "setup.h"
 } // setup end 
 
 #include "gps_functions.h" // gps functions
 
 void loop() 
 {
-    // picture loop - https://github.com/olikraus/u8glib/wiki/tpictureloop
-    OLED.firstPage();  // https://github.com/olikraus/u8glib/wiki/userreference#firstpage
-    do 
+     m2.checkKey();
+
+    if (m2.handleKey() != 0)
     {
-      draw(); // defined in functions.h
-    }
-    while( OLED.nextPage() ); // https://github.com/olikraus/u8glib/wiki/userreference#nextpage
-      
+      // picture loop - https://github.com/olikraus/u8glib/wiki/tpictureloop
+      OLED.firstPage();  // https://github.com/olikraus/u8glib/wiki/userreference#firstpage
+      do 
+      {
+        draw(); // defined in functions.h
+      }
+      while( OLED.nextPage() ); // https://github.com/olikraus/u8glib/wiki/userreference#nextpage
+    } 
+    
   handle_bluetooth_button(); // handles the bluetooth power button
   
   handle_lcd_buttons(); // handles the lcd buttons
