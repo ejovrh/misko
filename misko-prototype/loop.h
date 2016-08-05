784d784a1f6d72c0bbe83e8e2246e93e2f787ece
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-05 14:31:57 +0200

    - moved items from the actual loop.h() to loop.h in order to improve readability

---
+++ misko-prototype/loop.h
@@ -0,0 +1,20 @@
+     m2.checkKey();
+
+    if (m2.handleKey() != 0)
+    {
+      // picture loop - https://github.com/olikraus/u8glib/wiki/tpictureloop
+      OLED.firstPage();  // https://github.com/olikraus/u8glib/wiki/userreference#firstpage
+      do 
+      {
+        draw(); // defined in functions.h
+      }
+      while( OLED.nextPage() ); // https://github.com/olikraus/u8glib/wiki/userreference#nextpage
+    } 
+    
+  handle_bluetooth_button(); // handles the bluetooth power button
+  
+  handle_lcd_buttons(); // handles the lcd buttons
+  
+  get_nmea_sentences(); // gets NMEA sentences out of the GPS and deals with them
+
+
