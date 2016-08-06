3cad11d07e103517e59461db0c8abb1065249559
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-06 19:21:58 +0200

    - commented out Serial3.print(GSV_ON)

--- misko-prototype/setup.h
+++ misko-prototype/setup.h
@@ -1,7 +1,6 @@
 #include "pin_modes.h"
 #include "eeprom_config.h"
 
-
 Serial.begin(SERIALRATE); // connect to the serial terminal
 Serial.println("start");
 
@@ -11,7 +10,7 @@ Serial3.print(SERIAL_SET); // set gps serial comm. parameter
 Serial3.print(WAAS_ON); // will become obsolete via EEPROM and setup
 Serial3.print(RMC_ON); // will become obsolete via EEPROM and setup
 Serial3.print(GSA_ON); // will become obsolete via EEPROM and setup
-Serial3.print(GSV_ON); // will become obsolete via EEPROM and setup
+//Serial3.print(GSV_ON); // will become obsolete via EEPROM and setup
     
 delay(50);
   
