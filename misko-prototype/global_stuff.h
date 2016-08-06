c3b8a85908baa119c58a6291bb71f94ddcd1cc8d
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-06 23:17:43 +0200

    - added const char gpgga[] for $GPGGA sentence recognition - added char gps_hdop[5] and char gps_satellites_in_view[4] for GPS HDOP and satellites in view - modified display

--- misko-prototype/global_stuff.h
+++ misko-prototype/global_stuff.h
@@ -11,14 +11,17 @@
 char NMEA_buffer[NMEA_BUFFERSIZE] = "";        // string buffer for the NMEA sentence
 unsigned int bufferid = 0; // holds the current position in the NMEA_buffer array, used fo walk through the buffer
 const char gprmc[] = "$GPRMC"; // beginning of GPRMC sentence, used to fish out datetime via memcmp in gps_functions.h
+const char gpgga[] = "$GPGGA"; // beginning of GPGGA sentence, used to fish out datetime via memcmp in gps_functions.h 
 bool gps_fix = 0;
-char gps_date[9] = "20XXXXXX"; // 0-7 + 1 for '\0'
+char gps_date[9] = "20XXXXXX"; // 0-7 + 1 for '\0' -- YEAR 2100-BUG, HERE WE COME!!!
 char gps_time[7] = "XXXXXX"; // 0-5 + 1 for '\0'
-char gps_utc[7] = "UTC+0 "; // timezone string
+char gps_utc[7] = "UTC+2"; // timezone string
 char gps_logfile[13] = "";
-char gps_latitude[15] = "lat hhmm.ssss "; // N or S
-char gps_longtitude[16] = "lon hhhmm.ssss "; // W or E
-char gps_altitude[6] = "0000m";
+char gps_latitude[15] = "lat hhmm.ssss "; // N or S, memcpy needs to start to write at pos 4
+char gps_longtitude[16] = "lon hhhmm.ssss "; // W or E, memcpy needs to start to write at pos 4
+char gps_altitude[6] = ""; // GPS altitude
+char gps_hdop[5] = "D"; // GPS horizontal dilution of position
+char gps_satellites_in_view[4] = "S"; // GPS satellites in view
 
 // initalize u8g object
 //U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK);  // Display which does not send AC
@@ -44,29 +47,47 @@ bool flag_lcd_button_right_pressed = 0; // flag marks button pressed or not
 // display construction
 M2_LABEL(el_space, "f0", ' '); // label for space character
 
+// 1st line begin
 M2_LABEL(el_gps_date, "rf0", gps_date); // label for gps_date
 M2_LABEL(el_gps_time, "rf0", gps_time); // label for gps_time
 M2_LABEL(el_gps_utc, "rf0", gps_utc); // timezone string
 M2_LIST(datetime) { &el_gps_date, &el_space, &el_gps_time, &el_gps_utc} ; // create a list of gps date, time, timezone
-
 M2_HLIST(el_1st_line, NULL, datetime); // 1st line
-M2_LABEL(el_2nd_line, "rf0", gps_latitude); // 2nd line
-M2_LABEL(el_3rd_line, "rf0", gps_longtitude); // 3rd line
+// 1st line end
 
-M2_LABEL(el_altitude, "f0", "alt 123m");
-M2_LABEL(el_sat_in_view, "f0", "sat 12");
-M2_LIST(el_alt_sat) = {&el_altitude, &el_space, &el_sat_in_view};
+// 2nd line begin
+M2_LABEL(el_gps_latitude, "rf0", gps_latitude); 
+M2_LABEL(el_gps_hdop, "rf0", gps_hdop);
+M2_LIST(el_lat_hdop) = {&el_gps_latitude, &el_space, &el_gps_hdop};
+M2_HLIST(el_2nd_line, NULL, el_lat_hdop); // 2nd line
+// 2nd line end
+
+// 3rd line begin
+M2_LABEL(el_gps_longtitude, "rf0", gps_longtitude);
+M2_LABEL(el_gps_sat_in_view, "rf0", gps_satellites_in_view);
+M2_LIST(el_lon_sat) = {&el_gps_longtitude, &el_space, &el_gps_sat_in_view};
+M2_HLIST(el_3rd_line, NULL, el_lon_sat); // 3rd line
+// 3rd line begin
+
+// 4th line begin
+M2_LABEL(el_altitude, "rf0", "alt 123m");
+M2_LIST(el_alt_sat) = {&el_altitude};
 M2_HLIST(el_4th_line, NULL, el_alt_sat); // 4th line
+// 4th line begin
 
-M2_LABEL(el_batt_a, "fo", "batA 100%");
-M2_LABEL(el_batt_b, "fo", "batB 100%");
+// 5th line begin
+M2_LABEL(el_batt_a, "rfo", "batA 100%");
+M2_LABEL(el_batt_b, "rfo", "batB 100%");
 M2_LIST(el_battery) = {&el_batt_a, &el_space, &el_batt_b};
 M2_HLIST(el_5th_line, NULL, el_battery); // 5th line
+// 5th line begin
 
-M2_LABEL(el_bluetooth, "f0", "bt");
-M2_LABEL(el_accel, "f0", "accel");
+// 6th line begin
+M2_LABEL(el_bluetooth, "rf0", "bt");
+M2_LABEL(el_accel, "rf0", "accel");
 M2_LIST(el_misc) = {&el_bluetooth, &el_space, &el_accel};
-M2_HLIST(el_6th_line, NULL, el_misc); // 5th line
+M2_HLIST(el_6th_line, NULL, el_misc); // 6th line
+// 6th line begin
 
 M2_LIST(el_lines) = {&el_1st_line, &el_2nd_line, &el_3rd_line, &el_4th_line, &el_5th_line, &el_6th_line}; // line up elements
 
