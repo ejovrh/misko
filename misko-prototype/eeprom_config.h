a99a8ce9ac80272998a5653936ab983692a9d72d
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-06 22:40:46 +0200

    - replaced numbered eeprom indices with define-ed values

--- misko-prototype/eeprom_config.h
+++ misko-prototype/eeprom_config.h
@@ -17,16 +17,12 @@
 if (EEPROM[0] != CONFIG_VERSION) // only if the version did change we will write into the EERPOM
 {
   EEPROM[0] = CONFIG_VERSION; // indicates changes in structure, increment by one in case of any change
-  EEPROM[1] = 'a'; // lcd power - on(1), off(0), auto(a), default auto
-  EEPROM[2] = 'a'; // bluetooth power - on(1), off(0), auto(a), default auto
-  EEPROM[3] = 2; // lcd auto timeout - auto timeout in seconds, 0, 1, ..., 254, default 2s
-  EEPROM[4] = 5; // bluetooth auto timeout - auto timeout in seconds, 0, 1, ..., 254, default 60s
-  EEPROM[5] = 2; // UTC timezone, -12, ..., +12, default +2
-  EEPROM[6] = 1; // LOG_RMC - default on, RMC-Recommended Minimum Specific GNSS Data
-  EEPROM[7] = 0; // LOG_GGA - default off, GGA-Global Positioning System Fixed Data
-  EEPROM[8] = 0; // LOG_GLL - default off, GLL-Geographic Position-Latitude/Longitude
-  EEPROM[9] = 1; // LOG_GSA - default on, GSA-GNSS DOP and Active Satellites 
-  EEPROM[10] = 1; // LOG_GSV - default on, GSV-GNSS Satellites in View
-  EEPROM[11] = 0; // LOG_VTG - default off, VTG-Course Over Ground and Ground Speed
-  EEPROM[12] = 1; // USE_WAAS - default on
+  EEPROM[EERPOM_LCD_POWER_INDEX] = 'a'; // lcd power - on(1), off(0), auto(a), default auto
+  EEPROM[EERPOM_BLUETOOTH_POWER_INDEX] = 'a'; // bluetooth power - on(1), off(0), auto(a), default auto
+  EEPROM[EERPOM_LCD_AUTO_TIMEOUT_INDEX] = 2; // lcd auto timeout - auto timeout in seconds, 0, 1, ..., 254, default 2s
+  EEPROM[EERPOM_BLUETOOTH_ATUO_TIMEOUT_INDEX] = 5; // bluetooth auto timeout - auto timeout in seconds, 0, 1, ..., 254, default 60s
+  EEPROM[EERPOM_TIMEZONE_INDEX] = 2; // UTC timezone, -12, ..., +12, default +2
+  EEPROM[EEPROM_GPS_GPRMC_INDEX] = 1; // LOG_RMC - default on, RMC-Recommended Minimum Specific GNSS Data
+  EEPROM[EEPROM_GPS_GPGGA_INDEX] = 1; // LOG_GGA - default off, GGA-Global Positioning System Fixed Data
+  EEPROM[EEPROM_GPS_USE_WAAS_INDEX] = 1; // USE_WAAS - default on
 }
