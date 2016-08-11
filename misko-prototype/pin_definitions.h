e3c5b190a4f08ceb05fcd8914e7f788e6b296aa5
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-11 23:15:11 +0200

    - marked pins 10-13 not to be used since they are still connected to the level shifter on the gps v1.1 shield. previously pins 10-13 were used for software SPI on the mega2560 board (on the UNO they were genuine hardware SPI ports offered by the board). for init details see the SPI library implementation, chiefly Sd2Card.h:47 - defined spi_ss_sd_card pin for SD card SPI SS

--- misko-prototype/pin_definitions.h
+++ misko-prototype/pin_definitions.h
@@ -12,10 +12,10 @@
 // UART Serial 3
 //free
 
-#define sd_ss_pin 10 // SD/SPI slave select
-#define sd_sck_pin 11 // SD/SPI serial clock
-#define sd_miso_pin 12 // SD/SPI master in slave out
-#define sd_mosi_pin 13 // SD/SPI master out slave in
+#define DO_NOT_USE_pin 10 // was SW SPI SD/SPI slave select - see Sd2Card.h:47
+#define DO_NOT_USE_pin 11 // SD/SPI serial clock
+#define DO_NOT_USE_pin 12 // SD/SPI master in slave out
+#define DO_NOT_USE_pin 13 // SD/SPI master out slave in
 
 //#define oled_scl_pin 21 // OLED SCL
 //#define oled_sda_pin 20 // OLED SDA - MOSI
@@ -24,6 +24,7 @@
 #define menu_up_buttton 32 // user menu - up button
 #define menu_right_buttton 33 // user menu - right button
 #define bluetooth_power_toggle_pin 40 // user menu - bluetooth power toggle
+#define spi_ss_sd_card 45 // SD card SPI SS pin
 #define spi_adxl345_ss_pin 46 // slave select for adxl345
 #define spi_oled_a0_pin 47 // OLED address0 pin
 #define bluetooth_mosfet_gate_pin 48 // bluetooth power control via mosfet
@@ -32,4 +33,3 @@
 #define spi_mosi_pin 51 // SPI - Master Out Slave In - orange - connected are: OLED, ADXL345
 #define spi_sck_pin 52 // SPI - Serial Clock - green - connected are: OLED, ADXL345
 #define spi_ss_pin 53 // SPI - Slave Select - blue - connected are: OLED, ADXL345
-
