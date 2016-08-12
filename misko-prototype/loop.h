64e4d8b29f19f311af2b572c1e7ae795dcccae7b
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-13 01:05:18 +0200

    - added call to calculate_temperature()

--- misko-prototype/loop.h
+++ misko-prototype/loop.h
@@ -1,22 +1,22 @@
-    //Serial.println(millis());
-    m2.checkKey();
+ //Serial.println(millis());
+ m2.checkKey();
 
-    if (m2.handleKey() != 0)
-    {
-      // picture loop - https://github.com/olikraus/u8glib/wiki/tpictureloop
-      OLED.firstPage();  // https://github.com/olikraus/u8glib/wiki/userreference#firstpage
-      do 
-      {
-        m2.draw(); // defined in functions.h
-      }
-      while( OLED.nextPage() ); // https://github.com/olikraus/u8glib/wiki/userreference#nextpage
-    } 
-  //Serial.println(millis());
+ if (m2.handleKey() != 0)
+ {
+	 // picture loop - https://github.com/olikraus/u8glib/wiki/tpictureloop
+     OLED.firstPage();  // https://github.com/olikraus/u8glib/wiki/userreference#firstpage
+     do 
+     {
+		m2.draw(); // defined in functions.h
+     }
+     while( OLED.nextPage() ); // https://github.com/olikraus/u8glib/wiki/userreference#nextpage
+ } 
+ //Serial.println(millis());
     
-  handle_bluetooth_button(); // handles the bluetooth power button
+ handle_bluetooth_button(); // handles the bluetooth power button
   
-  //Serial.println(millis());
-  get_nmea_sentences(); // gets NMEA sentences out of the GPS and deals with them
-  //Serial.println(millis());
+ //Serial.println(millis());
+ get_nmea_sentences(); // gets NMEA sentences out of the GPS and deals with them
+ //Serial.println(millis());
   
-
+calculate_temperature();
