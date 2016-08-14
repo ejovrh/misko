4205f68459cdc33d52c692b7b34daf41e8b749aa
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-14 11:07:28 +0200

    - declated unsigned long's as uint32_t - reduced the temp printout time to 10 seconds for further debugging

--- misko-prototype/global_variables.h
+++ misko-prototype/global_variables.h
@@ -2,7 +2,7 @@
 #define CONFIG_VERSION 3 // protection against excessive EEPROM writes
 //   CONFIG_VERSION MUST BE CHANGED IF ANY CHANGES ARE MADE IN setup.h
 
-#define TEMPERATURE_SAMPLE_PERIOD 120 // temperature measure interval in seconds
+#define TEMPERATURE_SAMPLE_PERIOD 10 // temperature measure interval in seconds
 #define GPSRATE 4800
 #define SERIALRATE 9600
 //BLUETOOTHSERIALRATE is hardcoded in device
@@ -38,14 +38,14 @@ int8_t timezone;
 char sd_buffer[SD_BUFFERSIZE];
 
 // bluetooth flags
-unsigned long bluetooth_button_press_time = millis(); // time of button press
-unsigned long bluetooth_button_release_time = 0; // time of button release
+uint32_t bluetooth_button_press_time = millis(); // time of button press
+uint32_t bluetooth_button_release_time = 0; // time of button release
 bool flag_bluetooth_is_on = 0; // flag is BT device is powered on or off
 bool flag_bluetooth_power_toggle_pressed = 0; // flag marks bluetooth button pressed or not - used to recognize button state change for proper high/low handling
 bool flag_bluetooth_power_keep_on = 0;
 
 // LCD flags
-unsigned long lcd_button_press_time = millis(); // time of button press
+uint32_t lcd_button_press_time = millis(); // time of button press
 bool flag_lcd_is_on = 0; // flag is BT device is powered on or off
 bool flag_lcd_button_down_pressed = 0; // flag marks button pressed or not
 bool flag_lcd_button_up_pressed = 0; // flag marks button pressed or not
@@ -60,4 +60,4 @@ Sd2Card card;
 SdVolume volume;
 SdFile root;
 
-unsigned long temperature_last_reading = 0;
+uint32_t temperature_last_reading = 0;
