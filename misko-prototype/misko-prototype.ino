7a4bd90917fa7a13c0667cd5dfa281f08ca5bf97
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-15 23:12:00 +0200

    - added an interrupt handler routine for the ADXL345 - basic interrupt handling now works (the firmware reports activity or inactivity based upon accelerometer interrupts via Serial.print() - crude but a good 1st step)

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -6,8 +6,13 @@
 #include <U8glib.h> // https://github.com/olikraus/u8glib/wiki/userreference
 #include "M2tk.h" // https://code.google.com/archive/p/m2tklib/
 #include "utility/m2ghu8g.h" // the u8g graphics handler
+#include <avr/sleep.h>
+#include <avr/power.h>
 
 // local files
+
+#include "adxl345.h" // definition of ADXL345 register addresses
+
 #include "gps_config.h" // gps stuff
 #include "pin_definitions.h" // pin layout
 #include "global_variables.h" // global variables et al.
@@ -16,6 +21,8 @@
 
 void setup() 
 {
+	#include "eeprom_config.h" // definition of EEPROM fields
+	#include "pin_modes.h"
   #include "setup.h"
 }
 
@@ -25,3 +32,9 @@ void loop()
 {
   #include "loop.h"
 } 
+
+//ISR for the ADXL345 accelerometer
+void isr_adxl345_int1(void) 
+{
+	adxl345_int1 = 1;
+}
