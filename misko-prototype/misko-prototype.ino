97d90b9c5cd6d5d4e0b3abf6ba04486a8e66c6a5
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-15 23:16:45 +0200

    - added comments & removed blank lines

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -6,13 +6,11 @@
 #include <U8glib.h> // https://github.com/olikraus/u8glib/wiki/userreference
 #include "M2tk.h" // https://code.google.com/archive/p/m2tklib/
 #include "utility/m2ghu8g.h" // the u8g graphics handler
-#include <avr/sleep.h>
-#include <avr/power.h>
+#include <avr/sleep.h> // AVR sleep management
+#include <avr/power.h> // AVR power saving
 
 // local files
-
 #include "adxl345.h" // definition of ADXL345 register addresses
-
 #include "gps_config.h" // gps stuff
 #include "pin_definitions.h" // pin layout
 #include "global_variables.h" // global variables et al.
@@ -22,19 +20,18 @@
 void setup() 
 {
 	#include "eeprom_config.h" // definition of EEPROM fields
-	#include "pin_modes.h"
-  #include "setup.h"
+	#include "pin_modes.h" // definiton of pin modes
+  #include "setup.h" // the actual setup [ without () ]
 }
 
 #include "gps_functions.h" // gps functions
 
 void loop() 
 {
-  #include "loop.h"
+  #include "loop.h" // the actual loop [ without () ] 
 } 
 
-//ISR for the ADXL345 accelerometer
-void isr_adxl345_int1(void) 
+void isr_adxl345_int1(void) //ISR for the ADXL345 accelerometer
 {
-	adxl345_int1 = 1;
+	adxl345_int1 = 1; // simply (for now) set a flag to true (is false by default)
 }
