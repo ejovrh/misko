75ef2812f4952181b154668c2dcbc8d8a708f595
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-17 23:23:05 +0200

    - implemented cross-serial talk for SIM800L (NOT WORKING YET)

--- misko-prototype/loop.h
+++ misko-prototype/loop.h
@@ -7,7 +7,7 @@
      OLED.firstPage();  // https://github.com/olikraus/u8glib/wiki/userreference#firstpage
      do 
      {
-		m2.draw(); // defined in functions.h
+				m2.draw(); // defined in functions.h
      }
      while( OLED.nextPage() ); // https://github.com/olikraus/u8glib/wiki/userreference#nextpage
  } 
@@ -23,3 +23,11 @@ calculate_temperature(); // calculates the temperature via a TMP36 sensor
 
 if (adxl345_int1) // if the ADXL345 INT1 flag is set
 	handle_adx_intl(); // execute the function
+	
+//Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
+if(Serial2.available())
+	Serial.write(Serial2.read());
+
+//Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
+if(Serial.available())     
+    Serial2.write(Serial.read()); 
