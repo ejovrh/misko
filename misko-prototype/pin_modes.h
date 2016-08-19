b0becce808befbaeff9bc8f94edf5ca3b650e8ca
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-19 09:49:57 +0200

    - added MCP73871 status pin definitions

--- misko-prototype/pin_modes.h
+++ misko-prototype/pin_modes.h
@@ -2,7 +2,7 @@
 
 // PIN MODES
 pinMode(bat_A_pin, INPUT);
-pinMode(DO_NOT_USE_pin_10, OUTPUT);
+pinMode(DO_NOT_USE_pin_10, OUTPUT); // SPI SD SS
 pinMode(SPI_SS_ADXL345_pin, OUTPUT); // SPI SS for ADXL345
 pinMode(SPI_SS_SD_card_pin, OUTPUT); // SPI SS for the SD card
 pinMode(bluetooth_mosfet_gate_pin, OUTPUT); // bluetooth power control, bluetooth module draws ~40mA when not connected, 20-30 in connected state
@@ -10,12 +10,16 @@ pinMode(bluetooth_power_toggle_pin, INPUT); // user buttons - bluetooth power to
 pinMode(gps_green_led_pin, OUTPUT); // GPS fix indiator: on - has fix, off - has no fix
 pinMode(gps_power_pin, OUTPUT); // powers the gps (PWM) -- FIXME: direct power through the atmega2560; better is using a mosfet, draws ~4mA regardless of fix
 pinMode(gps_red_led_pin, OUTPUT); // write cycle to SD card indicator (blinks on write), on by default until GPS gets fix, then off
+pinMode(MCP73871_battery_low_pin, INPUT); // MCP73871 battery low indicator
+pinMode(MCP73871_charge_status_1_pin, INPUT); // MCP73871 charge status indicator 1
+pinMode(MCP73871_charge_status_2_pin, INPUT); // MCP73871 charge status indicator 2
+pinMode(MCP73871_power_good_indicator_pin, INPUT); // MCP73871 power good indicator
 pinMode(menu_down_buttton, INPUT); // user buttons - down
 pinMode(menu_left_buttton, INPUT); // user buttons - left
 pinMode(menu_left_buttton, INPUT); // user buttons - left 
 pinMode(menu_right_buttton, INPUT); //  user buttons - right
 pinMode(menu_up_buttton, INPUT); //  user buttons - up
-pinMode(sim800l_mosfet_gate_pin, OUTPUT); // SIM800L power control, connected directly to battery ( due to up 2A current )
+pinMode(SIM800L_mosfet_gate_pin, OUTPUT); // SIM800L power control, connected directly to battery ( due to up 2A current )
 pinMode(sleep_indicator_pin, OUTPUT);
 pinMode(TMP36_shutdown_pin, OUTPUT); // TMP36 shutdown control
 
@@ -28,11 +32,15 @@ digitalWrite(bluetooth_power_toggle_pin, LOW); //
 digitalWrite(gps_green_led_pin, LOW); // off(low) by befault until GPS gets fix
 digitalWrite(gps_power_pin, LOW); // low(on), high(off) 
 digitalWrite(gps_red_led_pin, HIGH); // on(high) by default until gps gets fix, then off, blinks on SD card write
+digitalWrite(MCP73871_battery_low_pin, HIGH); // high impedance state
+digitalWrite(MCP73871_charge_status_1_pin, HIGH); // high impedance state
+digitalWrite(MCP73871_charge_status_2_pin, HIGH); // high impedance state
+digitalWrite(MCP73871_power_good_indicator_pin, HIGH); // high impedance state
 digitalWrite(menu_down_buttton, LOW); // on(high), low(off)
 digitalWrite(menu_left_buttton, LOW); // on(high), low(off)
 digitalWrite(menu_right_buttton, LOW); // on(high), low(off)
 digitalWrite(menu_up_buttton, LOW); // on(high), low(off)
-digitalWrite(sim800l_mosfet_gate_pin, LOW);  // mosfet gate for SIM800L module, on(high), off(low)
+digitalWrite(SIM800L_mosfet_gate_pin, LOW);  // mosfet gate for SIM800L module, on(high), off(low)
 digitalWrite(sleep_indicator_pin, LOW); // sleep LED, off by default
 digitalWrite(TMP36_shutdown_pin, HIGH); // on(high), low(off), on by default
 delay(1); // delay for the last command to settle
