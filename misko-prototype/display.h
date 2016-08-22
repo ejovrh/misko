ab1f109d35c73529e72f8419151e18b943f6b50d
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-22 22:59:19 +0200

    - added el_whitespace - changed gps dop and sat in view element - minor format changes across some elements - added gsm menu item - format changes for the whole menu

--- misko-prototype/display.h
+++ misko-prototype/display.h
@@ -13,6 +13,7 @@
 // display construction start
 
 M2_BUTTON(el_ok, "f4", "ok", fn_ok); // an ok button
+M2_SPACE(el_whitespace, "W6h10");
 // data output start
 
 // datetime and timezone
@@ -29,14 +30,16 @@ M2_LABEL(el_gps_longtitude, "rf0", gps_longtitude);
 M2_LABEL(el_altitude, "rf0", gps_altitude);
 M2_LABEL(el_gps_sat_in_view, "rf0", gps_satellites_in_view);
 M2_LABEL(el_gps_hdop, "rf0", gps_hdop);
-M2_LIST(el_position_list) = {&el_gps_latitude, &el_gps_longtitude, &el_altitude, &el_gps_sat_in_view, &el_gps_hdop, &el_ok};
+M2_LIST(el_sat_dop_list) = {&el_gps_sat_in_view, &el_whitespace, & el_gps_hdop};
+M2_HLIST(el_hlist_sat_dop, "rf0", el_sat_dop_list);
+M2_LIST(el_position_list) = {&el_gps_latitude, &el_gps_longtitude, &el_altitude, &el_hlist_sat_dop, &el_ok};
 M2_GRIDLIST(el_position_grid, "c1", el_position_list);
 M2_ALIGN(el_top_position_menu, "-1|1W64H64", &el_position_grid);
 
 // misc
 M2_LABELFN(el_power_good, "fr0", fn_cb_get_power_good_status);
 M2_LABELFN(el_batt_charge, "rf0", fn_cb_get_batt_charge_status);
-M2_LIST(el_bat_list) = {&el_power_good, &el_batt_charge};
+M2_LIST(el_bat_list) = {&el_power_good, &el_whitespace, &el_batt_charge};
 M2_HLIST(el_batt_hlist, "rf0", el_bat_list);
 M2_LABELFN(el_batt_a, "rf0", fn_cb_get_bat_pct);
 M2_LABELFN(el_vcc, "rf0", fn_cb_get_Vcc);
@@ -61,7 +64,7 @@ M2_ALIGN(el_top_timezone_menu, "-1|1W64H64", &el_timezone_grid);
 // bluetooth start
 M2_LABEL(el_bluetooth_power, "rf0", "Power");
 M2_COMBOFN(el_bluetooth_power_value, "rf0", 3, fn_cb_bluetooth_power_setting);
-M2_S8NUMFN(el_bluetooth_power_timeout, "+0c1", 1, 5, fn_cb_set_eerpom_bluetooth_timeout);
+M2_S8NUMFN(el_bluetooth_power_timeout, "+0c1f0", 1, 5, fn_cb_set_eerpom_bluetooth_timeout);
 M2_LIST(el_bluetooth_list) = { &el_bluetooth_power, &el_bluetooth_power_value, &el_bluetooth_power_timeout, &el_ok };
 M2_GRIDLIST(el_bluetooth_grid, "c2", el_bluetooth_list);
 M2_ALIGN(el_top_bluetooth_menu, "-1|1W64H64", &el_bluetooth_grid);
@@ -70,7 +73,7 @@ M2_ALIGN(el_top_bluetooth_menu, "-1|1W64H64", &el_bluetooth_grid);
 // lcd start
 M2_LABEL(el_lcd_power, "rf0", "Power");
 M2_COMBOFN(el_lcd_power_value, "rf0", 2, fn_cb_lcd_power_setting);
-M2_S8NUMFN(el_lcd_power_timeout, "+0c1", 1, 5, fn_cb_set_eerpom_lcd_timeout);
+M2_S8NUMFN(el_lcd_power_timeout, "+0c1f0", 1, 5, fn_cb_set_eerpom_lcd_timeout);
 M2_LIST(el_lcd_list) = { &el_lcd_power, &el_lcd_power_value, &el_lcd_power_timeout, &el_ok };
 M2_GRIDLIST(el_lcd_grid, "c2", el_lcd_list);
 M2_ALIGN(el_top_lcd_menu, "-1|1W64H64", &el_lcd_grid);
@@ -78,12 +81,21 @@ M2_ALIGN(el_top_lcd_menu, "-1|1W64H64", &el_lcd_grid);
 
 // gps start
 M2_LABEL(el_gps_freq, "rf0", "Frequency");
-M2_S8NUMFN(el_gps_freq_value, "+0c2", 1, 20, fn_cb_set_eerpom_gps_log_freq);
+M2_S8NUMFN(el_gps_freq_value, "+0c1f0", 1, 20, fn_cb_set_eerpom_gps_log_freq);
 M2_ROOT(el_gps_ok, "f0", "OK", &top_el_expandable_menu);
 M2_LIST(el_gps_list) = { &el_gps_freq, &el_gps_freq_value, &el_gps_ok };
 M2_GRIDLIST(el_gps_grid, "c2", el_gps_list);
 M2_ALIGN(el_top_gps_menu, "-1|1W64H64", &el_gps_grid);
 // gps end
+
+// gsm start
+M2_COMBOFN(el_gsm_power, "rf0", 2, fn_cb_gsm_power);
+M2_ROOT(el_gsm_ok, "f0", "OK", &top_el_expandable_menu);
+
+M2_LIST(el_gsm_list) = { &el_gsm_power, &el_gsm_ok };
+M2_GRIDLIST(el_gsm_grid, "c2", el_gsm_list);
+M2_ALIGN(el_top_gsm_menu, "-1|1W64H64", &el_gsm_grid);
+// gsm end
 
 m2_menu_entry m2_2lmenu_data[] = 
 {
@@ -96,16 +108,16 @@ m2_menu_entry m2_2lmenu_data[] =
   { ". Bluetooth", &el_top_bluetooth_menu },
   { ". Display", &el_top_lcd_menu },
   { ". GPS", &el_top_gps_menu },
-	{ ". GSM", NULL},
+	{ ". GSM", &el_top_gsm_menu },
   { NULL, NULL },
 };
 
 uint8_t m2_2lmenu_first; // first visible line
 uint8_t m2_2lmenu_cnt; // total visible lines
 
-M2_2LMENU(el_2lmenu,"l4F3e15W43",&m2_2lmenu_first,&m2_2lmenu_cnt, m2_2lmenu_data,65,102,'\0');
+M2_2LMENU(el_2lmenu,"l5f0e0W55",&m2_2lmenu_first,&m2_2lmenu_cnt, m2_2lmenu_data,NULL,NULL,'\0');
 M2_SPACE(el_space, "W1h1");
-M2_VSB(el_vsb, "l4W2r1", &m2_2lmenu_first, &m2_2lmenu_cnt);
+M2_VSB(el_vsb, "l5w4r1", &m2_2lmenu_first, &m2_2lmenu_cnt);
 M2_LIST(list_2lmenu) = { &el_2lmenu, &el_space, &el_vsb };
 M2_HLIST(el_hlist, NULL, list_2lmenu);
 M2_ALIGN(top_el_expandable_menu, "-1|1W64H64", &el_hlist);
