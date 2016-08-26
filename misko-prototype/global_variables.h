581cbd2cb3043b41309fde703fd5a4ea57da8a69
ejovrh <hrvoje.ivanic@gmx.net>
2016-08-26 18:33:03 +0200

    - added some comments

--- misko-prototype/global_variables.h
+++ misko-prototype/global_variables.h
@@ -2,13 +2,15 @@
 #define CONFIG_VERSION 4 // protection against excessive EEPROM writes
 //   CONFIG_VERSION MUST BE CHANGED IF ANY CHANGES ARE MADE IN setup.h
 
+#define NMEA_DEBUG_PRINT 0 // define not 0 to have NMEA printout
+
+
 #define AREF_VOLTAGE 4.27
-#define NMEA_DEBUG_PRINT 1
 #define TEMPERATURE_SAMPLE_PERIOD 10 // temperature measure interval in seconds
 #define GPSRATE 4800
 #define SERIALRATE 9600
 #define NMEA_BUFFERSIZE 82 // officially, NMEA sentences are at maximum 82 characters long (80 readable characters + \r\n)
-#define SD_BUFFERSIZE 512 // huge buffer for NMEA sentences to be written to SD card
+#define SD_BUFFERSIZE 1024 // huge buffer for NMEA sentences to be written to SD card
 
 // EERPOM indices
 #define EERPOM_LCD_POWER_INDEX 1
@@ -45,20 +47,19 @@ char temp[6] = "T+xxC";
 uint16_t avg_temp = 0;
 char vcc[9] = "Vccx.xxV";
 char bat_a_pct[9] = "batAxxx%";
-char sd_buffer1[SD_BUFFERSIZE];
-char sd_buffer2[SD_BUFFERSIZE];
+char sd_buffer[SD_BUFFERSIZE];
 
 // bluetooth flags
 uint32_t bluetooth_button_press_time = millis(); // time of button press
 uint32_t bluetooth_button_release_time = 0; // time of button release
 bool flag_bluetooth_is_on = 0; // flag is BT device is powered on or off
 bool flag_bluetooth_power_toggle_pressed = 0; // flag marks bluetooth button pressed or not - used to recognize button state change for proper high/low handling
-bool flag_bluetooth_power_keep_on = 0;
+bool flag_bluetooth_power_keep_on = 0; // flag if the BT device shall be kept on and not power off on timeout
 
 // display variables
 uint32_t lcd_button_press_time = millis(); // time of button press
 bool flag_lcd_is_on = 0; // flag is BT device is powered on or off
-bool flag_oled_sleep = 0;
+bool flag_oled_sleep = 0; // flag if the OLED shall sleep or not
 M2_EXTERN_ALIGN(top_el_expandable_menu); // Forward declaration of the toplevel element
 
 // display device initializations
@@ -66,10 +67,10 @@ M2_EXTERN_ALIGN(top_el_expandable_menu); // Forward declaration of the toplevel 
 U8GLIB_SH1106_128X64_2X OLED(SPI_SS_OLED_pin,  SPI_OLED_a0_pin,  SPI_OLED_reset_pin); // HW SPI - look in the library source for precise info
 
 // set up variables using the SD utility library functions:
-File gpslogfile;
-bool flag_sd_write_enable = 0;
+File gpslogfile; // file object for the logfile
+bool flag_sd_write_enable = 0; // flag if a write shall be allowed or not - is controlled by log file name initialization
 
 uint16_t scheduler_run_count = 0;
 volatile bool flag_adxl345_int1 = 0;
-bool flag_gsm_on = 0;
+bool flag_gsm_on = 0; // flag if the gsm modem shall be powered on or not, controlled via menu combo, inspected in loop()
 uint8_t flag_cb_gsm_power = 0; // used in gsm power callback
