92263e03cf12ea1580863acf3b5f6c1bc0a4e0b2
ejovrh <hrvoje.ivanic@gmx.net>
2016-09-01 22:25:30 +0200

    - moved a few pins around since not all pins on the atmega1281 are accessible on the atmega2560

--- misko-prototype/pin_definitions.h
+++ misko-prototype/pin_definitions.h
@@ -3,9 +3,9 @@
 #define sleep_indicator_pin 7 // LED which lights up when the controller is sleeping
 
 // port A
-// PA0 - FREE (digital 22)
-// PA1 - FREE (digital 23)
-// PA2 - FREE (digital 24)
+																				// PA0 - FREE (digital 22)
+																				// PA1 - FREE (digital 23)
+																				// PA2 - FREE (digital 24)
 #define TMP36_shutdown_pin	25					// PA3 - shutdown pin on TMP36
 #define gps_power_pin 28 								// PA6 - supplies power via PWM (100% duty cycle) to em406a
 #define SIM800L_mosfet_gate_pin 29 			// PA7 - SIM800L power control via mosfet
@@ -35,16 +35,16 @@
 #define TWI_SDA_pin	20									// PD1 - TWI SDA
 #define USART1_RX 19  									// PD2 - Serial1 RX
 #define USART1_TX 18  									// PD3 - Serial1 TX
-// PD4 - FREE (!mega)
-// PD5 - ? (!mega)
-// PD6 - FREE (!mega)
-#define MCP73871_power_good_pin 38 			// PD7 - supply power indicator
+#define GPS_power_ctl_pin								// PD4 - FREE (!mega)
+#define GPS_reset_pin										// PD5 - GPS reset pin (!mega)
+#define MCP73871_power_good_pin 47 			// PL2 AS A STANDIN ON ATMEGA2560 // PD6 - supply power indicator
+#define MCP73871_charge_status_2_pin 38 // PL3 AS A STANDIN ON ATMEGA2560 // PD7 - charge status indicator 1 (charged)
 
 // port E
 #define PDI_USART0_RX										// PE0 - Programming Data Interface - Serial RX
 #define PDI_USART0_TX										// PE1 - Programming Data Interface - Serial TX
-// PE2 - ? (!mega)
-// PE3 - FREE (digital 5)
+																				// PE2 - ? (!mega)
+																				// PE3 - FREE (digital 5)
 #define SD_card_detect_pin 2 						// PE4 - SD card detect
 #define FRAM_hold_pin	3									// PE5 - FRAM hold
 #define FRAM_write_protect_pin					// PE6 - FRAM write protect
@@ -54,16 +54,16 @@
 #define TMP36_Vsense_pin A0 						// PF0 - TMP36 temoerature sensor 97
 #define bat_A_pin A1 										// PF1 - battery A voltage
 #define interrupt_adxl345_int1_pin 2 		// PF2 - ADXL345 INT1 pin
-// PF3 - FREE (analog 3)
+																				// PF3 - FREE (analog 3)
 #define JTAG_TCL_pin 4									// PF4 - JTAG TCL
 #define JTAG_TMS_pin 5									// PF5 - JTAG TMS
 #define JTAG_TDO_pin 6									// PF6 - JTAG TDO
 #define JTAG_TDI_pin 7									// PF7 - JTAG TDI
 
 // port G
-#define MCP73871_charge_status_2_pin 41 // PG0 - charge status indicator 1 (charged)
-#define MCP73871_charge_status_1_pin 40 // PG1 - charge status indicator 1 (charging / LBO)
+#define MCP73871_charge_status_1_pin 41 // PG0 - charge status indicator 1 (charging / LBO)
+																				// PG1 - FREE (digital 40)
 #define bluetooth_mosfet_gate_pin 39 		// PG2 - bluetooth power control via mosfet
-#define SPI_OLED_reset_pin 49 // PL0 AS A STANDIN ON ATMEGA2560			// PG3 (not routed on mega2560 !! ) - OLED reset pin (yellow)
-#define SPI_OLED_a0_pin 48 	 	// PL1 AS A STANDIN ON ATMEGA2560			// PG4 (not routed on mega2560 !! ) - OLED a0 (D/C) pin (white) 
+#define SPI_OLED_reset_pin 49 					// PL0 AS A STANDIN ON ATMEGA2560			// PG3 (not routed on mega2560 !! ) - OLED reset pin (yellow)
+#define SPI_OLED_a0_pin 48 	 						// PL1 AS A STANDIN ON ATMEGA2560			// PG4 (not routed on mega2560 !! ) - OLED a0 (D/C) pin (white) 
 // PG5 - FREE digital 4
