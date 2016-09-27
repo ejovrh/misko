c5af2486aadd8c8202633a89e64fa0930a9e7f1d
ejovrh <hrvoje.ivanic@gmx.net>
2016-09-27 13:17:03 +0200

    - added header for the fram

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -1,18 +1,19 @@
 // libraries
-#include <SoftwareSerial.h> // software serial library for SIM800L
+#include <SoftwareSerial.h> // software serial library for GPS/Bluetooth
 #include <SD.h> // SD card library
 #include <SPI.h> // SPI library
-#include <EEPROM.h> // EEPROM library
+#include <EEPROM.h> // EEPROM library for configuration data
 #include <U8glib.h> // OLED chip driver - https://github.com/olikraus/u8glib/wiki/userreference
-#include <M2tk.h> // menu library -  https://code.google.com/archive/p/m2tklib/
-#include "utility/m2ghu8g.h" // graphics handler (one of many)
+#include <M2tk.h> // user menu library - https://code.google.com/archive/p/m2tklib/
+#include "utility/m2ghu8g.h" // U8glib graphics handler (one of many)
 #include "utility/mas.h" // mass storage handler for the SD card
-#include <gprs.h> // GPRS libarary for the SIM800L
+#include <gprs.h> // GPRS library for SIM800C
 #include <avr/sleep.h> // AVR sleep management
 #include <avr/power.h> // AVR power saving
 
 // local files
 #include "adxl345.h" // definition of ADXL345 register addresses
+#include "fram.h" // definition of FRAM register addresses
 #include "pin_definitions.h" // pin layout
 #include "global_variables.h" // global variables et al.
 #include "functions.h" // useful functions
