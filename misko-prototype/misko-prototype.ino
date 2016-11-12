1c0bd19d932bf7db219cec463987a38d1a7f00a7
ejovrh <hrvoje.ivanic@gmx.net>
2016-11-12 12:33:13 +0100

    - added a few items to ISR(TIMER5_COMPA_vect) (variales (updated each second) which hold measurement data and which will be written to SD card) - changed INT7 ISR to read out INT_SOURCE (which in turn is processed in loop.h)

--- misko-prototype/misko-prototype.ino
+++ misko-prototype/misko-prototype.ino
@@ -39,13 +39,46 @@ void loop()
 //ISR for the ADXL345 accelerometer
 ISR(INT7_vect)
 {
-	flag_adxl345_int1 = 1; // simply (for now) set a flag to true (is false by default)
+	adxl345_irq_src = adxl345_readByte(INT_SOURCE);
 }
 
 // ISR for timer5 events - generated every second
 //	stuff that needs to run periodically goes here
 ISR(TIMER5_COMPA_vect)
 {
-	//avg_temperature(calculate_temperature(), 10); // calculates the temperature via a TMP36 sensor over 10 iterations
 	scheduler_run_count++;
+
+	val_vcc = 2 * calculate_voltage(Vcc_sense_pin); // measures Vcc across a voltage divider
+	val_temp = calculate_temperature(); // reads out temperature-dependant voltage
+		/* the percentage calculation
+		union battery datasheet: charge cutoff voltage: Vbat 4.20V, discharge cutoff voltage: Vbat 2.70V
+			over the voltage divider this gives 2.10V and 1.3V
+			our voltage divider gives 0.5 Vbat
+
+	 percentage calculation see https://racelogic.support/02VBOX_Motorsport/Video_Data_Loggers/Video_VBOX_Range/Video_VBOX_-_User_manual/24_-_Calculating_Scale_and_Offset
+
+		dX is 2.1 - 1.35 = 0.75
+		dY is 100 - 0 = 100
+	 the gradient is dX/dY = 133
+
+	 Y = percent = 0, X = Voltage = 1.35V
+	 	0 = ((dX/dY)* voltage) + c
+		0 = (133 * 1.35) + c <=> 0 = 180 + c <=> c = -180
+		our equation is: y = 133 * x - 180
+
+	elementary, dr. watson!
+ 	*/
+	val_batA_pct = (133 * calculate_voltage(bat_A_pin)) - 180; // calculus...
+	val_batB_pct = 000;
+
+	if(!flag_run_once)
+	{
+		flag_run_once = 1;
+
+		//SPI voodoo - for some reason it is necessary for the ADXL345 to "work"
+		SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE3));
+		adxl345_readByte(INT_SOURCE); // reads INT SRC to clear the act/inact bits
+		SPI.endTransaction();
+
+	}
 }
