17f1b17749856a8b1b09e9aaa2adca1a72fb5aed
ejovrh <hrvoje.ivanic@gmx.net>
2016-11-24 19:13:30 +0100

    - format changes, no relevant code changes

--- misko-prototype/gps_functions.h
+++ misko-prototype/gps_functions.h
@@ -75,6 +75,7 @@ void gps_parse_gprmc(char *in_str)
 		i++; // counter increment
 	}
 }
+
 // parses out satellites used, HDOP, MSL altitude
 void gps_parse_gpgga(char *in_str)
 {
@@ -140,107 +141,106 @@ void gps_parse_gpgga(char *in_str)
 	}
 }
 
-void get_nmea_sentences() {
-// reads one char at a time from the gps device;
-//  due to iteration from the main loop, eventually the whole NMEA sentence accumulates in the global buffer variable
+// takes an incoming NMEA sentence and acts upon it (parse for RMC and GGA, decides to log to file, etc.)
+void get_nmea_sentences()
+{
+	// reads one char at a time from the gps device;
+	//  due to iteration from the main loop, eventually the whole NMEA sentence accumulates in the global buffer variable
 
-  uint8_t sum; // variable for the NMEA checksum of each sentence
+	uint8_t sum; // variable for the NMEA checksum of each sentence
 
-  if (gps.available()) // if gps is availiable
-  {
-    *(NMEA_buffer+bufferid) = gps.read();      // put byte by byte into the array "buffer"
+	*(NMEA_buffer+bufferid) = gps.read();      // put byte by byte into the array "buffer"
 
-    if ( *(NMEA_buffer+bufferid) == '\n' )           // if we have recieved a newline -- read  http://www.gammon.com.au/forum/?id=11425
+	if ( *(NMEA_buffer+bufferid) == '\n' )           // if we have recieved a newline -- read  http://www.gammon.com.au/forum/?id=11425
                //it means we are at the end of the NMEA sentence and we can start to parse it (the GPS reciever will terminate each NMEA sentece with at '\n' )
-    {
-      *(NMEA_buffer+(bufferid+1) ) = '\0'; // terminate it with a NULL terminator
+	{
+		*(NMEA_buffer+(bufferid+1) ) = '\0'; // terminate it with a NULL terminator
 
-      if ( *(NMEA_buffer+(bufferid-4)) != '*' ) // no "*" indicating no checksum field
-      {
-        bufferid = 0;
-        return;  // break out of the loop and read next iterations
-      }
+		if ( *(NMEA_buffer+(bufferid-4)) != '*' ) // no "*" indicating no checksum field
+		{
+			bufferid = 0;
+			return;  // break out of the loop and read next iterations
+		}
 
-      sum = parseHex(NMEA_buffer[bufferid-3]) * 16; // sum the last fields for the checksum
-      sum += parseHex(NMEA_buffer[bufferid-2]);
+		sum = parseHex(NMEA_buffer[bufferid-3]) * 16; // sum the last fields for the checksum
+		sum += parseHex(NMEA_buffer[bufferid-2]);
 
-      for (uint8_t i=1; i < (bufferid-4); i++)
-        sum ^= *(NMEA_buffer+i); // check checksum
+		for (uint8_t i=1; i < (bufferid-4); i++)
+			sum ^= *(NMEA_buffer+i); // check checksum
 
-      if (sum != 0) // checksum bad
-      {
-        bufferid = 0; // set pointer back to the beginning
-        return;
-      }
+		if (sum != 0) // checksum bad
+		{
+			bufferid = 0; // set pointer back to the beginning
+			return;
+		}
 
-			//NMEA sentence printout
-				if ( (FeRAMReadByte(FERAM_GPS_MISC_CFG) >> FERAM_GPS_MISC_CFG_NMEA_PRINT_TO_SERIAL) & 0x01 ) // if the 2nd bit is set
-					Serial.print(NMEA_buffer);
+		//NMEA sentence printout
+		if ( (FeRAMReadByte(FERAM_GPS_MISC_CFG) >> FERAM_GPS_MISC_CFG_NMEA_PRINT_TO_SERIAL) & 0x01 ) // if the 2nd bit is set
+			Serial.print(NMEA_buffer);
 
-      // check for GPRMC sentence
-      if (strncmp(NMEA_buffer, "$GNRMC", 6) == 0) // if we have a GPRMC sentence (compare the NMEA buffer with its sentence to gprmc[])
+		// check for GPRMC sentence
+		if (strncmp(NMEA_buffer, "$GNRMC", 6) == 0) // if we have a GPRMC sentence (compare the NMEA buffer with its sentence to gprmc[])
+		{
+			gps_parse_gprmc(NMEA_buffer); // parse the GPRMC sentence and get datetime and other values
+
+			if (flag_gps_fix) // valid fix - indicate it by lighting up the red LED
+				digitalWrite(gps_green_led_pin, LOW);
+			else
+				digitalWrite(gps_green_led_pin, HIGH);
+		}
+
+		// check for GPGGA sentence
+		if (flag_gps_fix && strncmp(NMEA_buffer, "$GNGGA",  6) == 0) // if we have a GPRMC sentence
+			gps_parse_gpgga(NMEA_buffer); // get HDOP, altitude and satellites in view
+
+		bufferid++; // ?!? needed??
+
+		// start the write cycle
+	#if BUFFER_DEBUG_PRINT
+		if ( ( (FeRAMReadByte(FERAM_DEVICE_MISC_CFG1) >> FERAM_DEVICE_MISC_CFG1_SD_WRITE ) & 0x01) && flag_sd_write_enable) // if we are set up to write - i.e. the logfile name is set
+	#else
+		if ( ( (FeRAMReadByte(FERAM_DEVICE_MISC_CFG1) >> FERAM_DEVICE_MISC_CFG1_SD_WRITE ) & 0x01) && flag_sd_write_enable && flag_gps_fix) // if we are set up to write - i.e. the logfile name is set
+	#endif
+		{
+			// logfile name generation - should run only once a day
+			if (strlen(gps_date) != 2 && strstr(gps_logfile, gps_date) == NULL ) // if "gps_date is initialized" and "gps_logfile does not contain the current datetime (e.g. on startup or on date change)"
 			{
-        gps_parse_gprmc(NMEA_buffer); // parse the GPRMC sentence and get datetime and other values
-
-        if (flag_gps_fix) // valid fix - indicate it by lighting up the red LED
-          digitalWrite(gps_green_led_pin, LOW);
-        else
-          digitalWrite(gps_green_led_pin, HIGH);
-      }
-
-      // check for GPGGA sentence
-      if (flag_gps_fix && strncmp(NMEA_buffer, "$GNGGA",  6) == 0) // if we have a GPRMC sentence
-          gps_parse_gpgga(NMEA_buffer); // get HDOP, altitude and satellites in view
-
-			bufferid++; // ?!? needed??
-
-			// start the write cycle
-      #if BUFFER_DEBUG_PRINT
-			if ( ( (FeRAMReadByte(FERAM_DEVICE_MISC_CFG1) >> FERAM_DEVICE_MISC_CFG1_SD_WRITE ) & 0x01) && flag_sd_write_enable) // if we are set up to write - i.e. the logfile name is set
-			#else
-			if ( ( (FeRAMReadByte(FERAM_DEVICE_MISC_CFG1) >> FERAM_DEVICE_MISC_CFG1_SD_WRITE ) & 0x01) && flag_sd_write_enable && flag_gps_fix) // if we are set up to write - i.e. the logfile name is set
-			#endif
-      {
-				// logfile name generation - should run only once a day
-				if (strlen(gps_date) != 2 && strstr(gps_logfile, gps_date) == NULL ) // if "gps_date is initialized" and "gps_logfile does not contain the current datetime (e.g. on startup or on date change)"
-				{
-					//FIXME some weird behaviour
+				//FIXME some weird behaviour
 /*
-					strncat(gps_logfile, gps_date, 4*sizeof(char));//Serial.println(gps_logfile);
-					strcat(gps_logfile, "/");//Serial.println(gps_logfile);
-					strncat(gps_logfile, gps_date+4*sizeof(char), 2*sizeof(char));//Serial.println(gps_logfile);
+				strncat(gps_logfile, gps_date, 4*sizeof(char));//Serial.println(gps_logfile);
+				strcat(gps_logfile, "/");//Serial.println(gps_logfile);
+				strncat(gps_logfile, gps_date+4*sizeof(char), 2*sizeof(char));//Serial.println(gps_logfile);
 
-					if (!SD.exists(gps_logfile))
-						SD.mkdir(gps_logfile);
+				if (!SD.exists(gps_logfile))
+					SD.mkdir(gps_logfile);
 
-					strcat(gps_logfile, "/");//Serial.println(gps_logfile);
-		 */
-					strcat(gps_logfile,gps_date); // constrcut the logfile
-					strcat(gps_logfile, ".log");
+				strcat(gps_logfile, "/");//Serial.println(gps_logfile);
+*/
+				strcat(gps_logfile,gps_date); // constrcut the logfile
+				strcat(gps_logfile, ".log");
 
-					Serial.println(gps_logfile);
-				}
-				// open file in write mode - once! (not on every iteration)
-				if (!gpslogfile) // run only on initialization, not on every loop iteration
-						gpslogfile = SD.open(gps_logfile, FILE_WRITE);
+				Serial.println(gps_logfile);
+			}
 
-				sd_buffer_write(NMEA_buffer, bufferid); // write NMEA data into buffer
-      }
+			// open file in write mode - once! (not on every iteration)
+			if (!gpslogfile) // run only on initialization, not on every loop iteration
+				gpslogfile = SD.open(gps_logfile, FILE_WRITE);
 
-			if (strlen(gps_logfile) != 1) // check if the gps_logfile is of proper lenght (== likely to be initialized)
-				flag_sd_write_enable = 1; // flag the sd card as writeable because now we have a valid datetime set (needed for logfile name)
+			sd_buffer_write(NMEA_buffer, bufferid); // write NMEA data into buffer
+		}
 
-      bufferid = 0;    //reset buffer pointer
-      return;
-    } // if (c == '\n')
+		if (strlen(gps_logfile) != 1) // check if the gps_logfile is of proper lenght (== likely to be initialized)
+			flag_sd_write_enable = 1; // flag the sd card as writeable because now we have a valid datetime set (needed for logfile name)
 
-    bufferid++;
+		bufferid = 0;    //reset buffer pointer
+		return;
+	} // if (c == '\n')
 
-    if (bufferid == NMEA_BUFFERSIZE-1) // if we get to the end of the buffer, right before the '\0' terminator
-    {
-      bufferid = 0; // we want to put the "cursor" to the beginning of the buffer array
-      return;
-    }
+	bufferid++;
 
-  } // if (gps.available())
+	if (bufferid == NMEA_BUFFERSIZE-1) // if we get to the end of the buffer, right before the '\0' terminator
+	{
+		bufferid = 0; // we want to put the "cursor" to the beginning of the buffer array
+		return;
+	}
 } // void get_nmea_sentence()
