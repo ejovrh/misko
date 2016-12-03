d5fb8ba75c69a1043902715a9c93a3f0e09d1a51
ejovrh <hrvoje.ivanic@gmx.net>
2016-12-03 12:37:06 +0100

    - added more fields into FeRAM: FERAM_GPS_LAST_GOOD_POSITION, FERAM_GSM_APN, FERAM_GSM_HTTP_SERVER - added a bit of comments

--- misko-prototype/fram.h
+++ misko-prototype/fram.h
@@ -36,7 +36,7 @@
  *		represents the current config version
  *			to be changed if the code stucture changes
  */
- #define FERAM_CONFIG_VERSION												0x00	// byte, unsigned integer
+ #define FERAM_CONFIG_VERSION												0x00	// 1 byte, unsigned integer
 
 /*
  *	FERAM_DEVICE_TIMEZONE
@@ -44,7 +44,7 @@
  *
  *		timezone value
  */
- #define FERAM_DEVICE_TIMEZONE											0x01	// byte (-12 ... +12) -- hours
+ #define FERAM_DEVICE_TIMEZONE											0x01	// 1 byte (-12 ... +12) -- hours
 
 /*
  *	FERAM_OLED_AUTO_TIMEOUT
@@ -52,7 +52,7 @@
  *
  *		OLED device auto power-off timeout in seconds
  */
-#define FERAM_OLED_AUTO_TIMEOUT											0x02 // byte (1 ... 255) -- seconds
+#define FERAM_OLED_AUTO_TIMEOUT											0x02 // 1 byte (1 ... 255) -- seconds
 
 /*
  *	FERAM_BLUETOOTH_AUTO_TIMEOUT
@@ -60,7 +60,7 @@
  *
  *		Bluetooth device auto power-off timeout in seconds
  */
-#define FERAM_BLUETOOTH_AUTO_TIMEOUT								0x03 // byte (1 ... 255) -- seconds
+#define FERAM_BLUETOOTH_AUTO_TIMEOUT								0x03 // 1 byte (1 ... 255) -- seconds
 
 /*
  *	FERAM_ADXL345_MOVEMENT_TIMEOUT
@@ -68,7 +68,7 @@
  *
  *		ADXL345 timeout in seconds until sleep after last movement is detected
  */
-#define FERAM_ADXL345_MOVEMENT_TIMEOUT							0x04 // byte (1 ... 255) -- seconds
+#define FERAM_ADXL345_MOVEMENT_TIMEOUT							0x04 // 1 byte (1 ... 255) -- seconds
 
 /*
  *	FERAM_GPS_GPRMC_GPGGA_FREQ
@@ -77,7 +77,7 @@
  *			b0-b3: GPRMC frequency in seconds
  *			b4-b7: GPGGA frequency in seconds
  */
-#define FERAM_GPS_GPRMC_GGA_GSA_FREQ								0x05 // nibble (0 ... 15) -- seconds
+#define FERAM_GPS_GPRMC_GGA_GSA_FREQ								0x05 // 1 byte, holds 2 nibbles (0 ... 15) -- seconds
 #define FERAM_GPS_GPRMC_GGA_FREQ										0
 #define FERAM_GPS_GPGSA_FREQ												4
 
@@ -88,7 +88,7 @@
  *			b0-b3: GPGSA frequency in seconds
  *			b4-b7: GPGSV frequency in seconds
  */
-#define FERAM_GPS_GPZDA_GSV_FREQ										0x06 // nibble (0 ... 15) -- seconds
+#define FERAM_GPS_GPZDA_GSV_FREQ										0x06 // 1 byte, holds 2 nibbles (0 ... 15) -- seconds
 #define FERAM_GPS_GPZDA_FREQ												0
 #define FERAM_GPS_GPGSV_FREQ												4
 
@@ -104,7 +104,7 @@
  *			http://www.avrfreaks.net/forum/tut-c-bit-manipulation-aka-programming-101?page=all
  *			and many others
  */
-#define FERAM_GPS_MISC_CFG													0x07 // bit (0, 1)
+#define FERAM_GPS_MISC_CFG													0x07 // 1 byte, holds 8 bits (0, 1) -- flags
 #define FERAM_GPS_MISC_CFG_POWER_CTL								0 // GPS power - on(1) or off(0)
 #define FERAM_GPS_MISC_CFG_NMEA_PRINT_TO_SERIAL			1 // print NMEA sentences to serial - yes(1) or no(0)
 #define FERAM_GPS_MISC_CFG_bit3											2 // unassigned - on(1) or off(0)
@@ -120,7 +120,7 @@
  *
  *		miscellaneous boolean device settings
  */
-#define FERAM_DEVICE_MISC_CFG1											0x08 //
+#define FERAM_DEVICE_MISC_CFG1											0x08 // 1 byte, holds 8 bits (0, 1) -- flags
 #define FERAM_DEVICE_MISC_CFG1_OLED_AUTO_POWER			0 // enable OLED auto-poweroff - auto(0), constant on(1)
 #define FERAM_DEVICE_MISC_CFG1_ADXL345_AUTO_POWER		1	// allow the accelerometer to trigger a device sleep - yes(1), no(0)
 #define FERAM_DEVICE_MISC_CFG1_SD_WRITE							2 // write NMEA to SD card on valid fix - yes(1), no(0)
@@ -134,9 +134,9 @@
  *	FERAM_DEVICE_MISC_CFG2
  *		two bits, 0 (off), 1 (on), 2(auto) 3(if defined)
  *
- *		miscellaneous tristate device settings, e.g. on, off and something else
+ *		miscellaneous quad_state device settings, e.g. on, off, something and something else
  */
-#define FERAM_DEVICE_MISC_CFG2											0x09 //
+#define FERAM_DEVICE_MISC_CFG2											0x09 // 1 byte - holds 4 bit-pairs, values 0-3
 #define FERAM_DEVICE_MISC_CFG2_SYSTEM_SERIAL				0 // connection to serial console - GPS(0), SIM800(1), system(2)
 #define FERAM_DEVICE_MISC_CFG2_BLUETOOTH_POWER			2 // Bluetooth power scheme - off(0), on(1), auto(2)
 #define FERAM_DEVICE_MISC_CFG2_GPRS_FUNCTION				4 // GPRS module behaviour - nothing(0), push on demand(1), upload log(2), foo(3)
@@ -148,7 +148,7 @@
  *
  *		represents the activity threshold
  */
-#define FERAM_ADXL345_ACTIVITY_THRESHOLD						0x0A
+#define FERAM_ADXL345_ACTIVITY_THRESHOLD						0x0A	// 1 byte
 
 /*
  *	FERAM_ADXL345_INACTIVITY_THRESHOLD
@@ -156,13 +156,13 @@
  *
  *		represents the inactivity threshold
  */
-#define FERAM_ADXL345_INACTIVITY_THRESHOLD					0x0B
+#define FERAM_ADXL345_INACTIVITY_THRESHOLD					0x0B	// 1 byte
 
 /*
  *
  *
  */
-#define FERAM_ADXL345_FREE													0x0C //
+#define FERAM_ADXL345_FREE													0x0C	// 1 byte
 
 /*
  *	FERAM_GSM_MISC_CFG
@@ -171,9 +171,9 @@
  *		miscellaneous boolean GSM settings, mostly flag-type things
  *
  */
-#define FERAM_GSM_MISC_CFG													0x0D // bit (0, 1)
+#define FERAM_GSM_MISC_CFG													0x0D // 1 byte, holds 8 bits (0, 1) -- flags
 #define FERAM_GSM_MISC_CFG_POWER_CTL								0 // GSM  power - on(1) or off(0)
-#define FERAM_GSM_MISC_CFG_NMEA_PRINT_TO_SERIAL			1 // unassigned - on(1) or off(0)
+#define FERAM_GSM_MISC_CFG_bit2											1 // unassigned - on(1) or off(0)
 #define FERAM_GSM_MISC_CFG_bit3											2 // unassigned - on(1) or off(0)
 #define FERAM_GSM_MISC_CFG_bit4											3 // unassigned - on(1) or off(0)
 #define FERAM_GSM_MISC_CFG_bit5											4 // unassigned - on(1) or off(0)
@@ -181,3 +181,34 @@
 #define FERAM_GSM_MISC_CFG_bit7											6 // unassigned - on(1) or off(0)
 #define FERAM_GSM_MISC_CFG_bit8											7 // unassigned - on(1) or off(0)
 
+	/*
+	 *	FERAM_GSM_MISC_CFG
+	 *		83-byte field
+	 *
+	 *		contains last good GPS position as a NMEA sentece, which can be max. 82 + '\0' chars long
+	 */
+#define FERAM_GPS_LAST_GOOD_POSITION								0x0E	// 83 bytes, holds a string
+
+	/*
+	 *	FERAM_GSM_APN
+	 *		64-byte field
+	 *
+	 *		contains the APN, by protocol max 63 octets + '\0' long
+	 */
+#define FERAM_GSM_APN																0x62	// 64 bytes, holds a string
+
+	/*
+	 *	FERAM_GSM_HTTP_SERVER
+	 *		64-byte field
+	 *
+	 *		contains the remote HTTP server URL, by design decision max 63 octets + '\0' long
+	 */
+#define	FERAM_GSM_HTTP_SERVER												0xA2	// 64 bytes, holds a string
+
+	/*
+	 *	FERAM_
+	 *		foo-byte field
+	 *
+	 *		contains the APN, by protocol max 63 octets + '\0' long
+	 */
+#define FERAM_FREE																	0xE2 //
