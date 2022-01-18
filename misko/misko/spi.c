0502b8f851c513df3dc022db113bb30fdbf5415a
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2022-01-18 19:58:49 +0100

    in spi.c: - minor changes in comments - removed commented out buson/busoff functions

--- misko/misko/spi.c
+++ misko/misko/spi.c
@@ -2,7 +2,7 @@
 
 #include <avr/io.h>
 
-typedef struct																	// fm25w256_t actual
+typedef struct																	// spi_t actual
 {
 	// TODO - write semaphore code
 	uint8_t __semaphore;														// private var - semaphore for critical section
@@ -10,33 +10,7 @@ typedef struct																	// fm25w256_t actual
 	spi_t public;																// public struct
 } __spi_t;
 
-extern __spi_t __SPI __attribute__ ((section (".data")));						// preallocate __fm25w256 object in .data
-
-/*
-static inline void _SPIBusOn(void)												// SPI init - MSB first, mode 3, 4MHz
-{
-	SPCR = (
-				(1<<SPE) |														// SPI Enable
-				(0<<SPIE) |														// SPI Interrupt disable
-				(0<<DORD) |														// Data Order (0:MSB first / 1:LSB first)
-				(1<<MSTR) |														// configure as master
-				(0<<SPR1) |														// CLK shall be Fosc/(2*2) = 4MHz
-				(0<<SPR0) |														//
-				(1<<CPOL) |														// SPI mode 3
-				(1<<CPHA)														//
-			);
-
-	SPSR = (1<<SPI2X);															// Double Clock Rate
-
-	PRR0 &= ~_BV(PRSPI);														// unset bit - SPI bus power on
-};
-
-static inline void _SPIBusOff(void)												// SPI reset & power off
-{
-	PRR0 |= _BV(PRSPI);															// set bit - SPI bus power off
-	SPCR = 0x00;																// set the SPI control register to all 0
-};
-*/
+extern __spi_t __SPI __attribute__ ((section (".data")));						// preallocate __spi object in .data
 
 static inline uint8_t _TransferByte(const uint8_t in_val)						// sends a byte into the circular buffer and gets something back
 {
@@ -57,7 +31,7 @@ spi_t *spi_ctor(void)															// object constructor
 	__SPI.__semaphore = 0;														// semaphore for critical sections
 
 	__SPI.public.TransferByte = &_TransferByte;									// set function pointer
-	
+
 
 	return &__SPI.public;														// return address of public part; calling code accesses it via pointer
 };
