cadb753d6d7700c68bf18ee78fe569c0389a90fe
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2022-01-18 14:44:02 +0100

    SDCard.c/.h: - in preparation for driver development created object structure according to other similar "classes" - e.g. the accelerometer

--- misko/misko/SDCard/SDCard.c
+++ misko/misko/SDCard/SDCard.c
@@ -1,1 +1,39 @@
-#include "../spi.h"
+#include "SDCard/SDCard.h"
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "spi.h"
+#include "gpio.h"
+#include "gpio_definitions.h"
+
+typedef struct																	// sd_t actual
+{
+	spi_t *_spi;																// tie in SPI object
+
+	sd_t public;																// public struct
+} __sd_t;
+
+static __sd_t __SD __attribute__ ((section (".data")));							// preallocate __sd_t object in .data
+
+static void _Read(void)	//
+{
+	;
+};
+
+static void _Write(void)	//
+{
+	;
+};
+
+
+
+sd_t *sd_ctor(void)																// object constructor
+{
+	__SD._spi = spi_ctor();														// tie in SPI
+
+	__SD.public.Read = &_Read;													// set function pointer
+	__SD.public.Write = &_Write;												//	ditto
+
+	return &__SD.public;														// return address of public part; calling code accesses it via pointer
+};
--- misko/misko/SDCard/SDCard.h
+++ misko/misko/SDCard/SDCard.h
@@ -1,8 +1,16 @@
 #ifndef SDCARD_H_
 #define SDCARD_H_
 
+#include <inttypes.h>
 
+typedef struct																	// public part of struct describing the SD card
+{
+	void (*Read)(void);	//
+	void (*Write)(void);	//
+} sd_t;
 
+sd_t *sd_ctor(void);															// declare constructor for concrete class
 
+extern sd_t * const sd;															// declare pointer to public struct part
 
 #endif /* SDCARD_H_ */
