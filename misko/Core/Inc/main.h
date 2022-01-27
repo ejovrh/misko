f000d2a22784e8a7b3465e027ff4c5154890ec0b
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2022-01-27 21:30:10 +0100

    - configured LED pins - configured SPI1 pins

--- misko/Core/Inc/main.h
+++ misko/Core/Inc/main.h
@@ -57,12 +57,26 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define B1_Pin GPIO_PIN_13
-#define B1_GPIO_Port GPIOC
+#define PushButton_Pin GPIO_PIN_13
+#define PushButton_GPIO_Port GPIOC
+#define LED105_Pin GPIO_PIN_15
+#define LED105_GPIO_Port GPIOC
 #define USART_TX_Pin GPIO_PIN_2
 #define USART_TX_GPIO_Port GPIOA
 #define USART_RX_Pin GPIO_PIN_3
 #define USART_RX_GPIO_Port GPIOA
+#define LED107_Pin GPIO_PIN_5
+#define LED107_GPIO_Port GPIOA
+#define LED104_Pin GPIO_PIN_6
+#define LED104_GPIO_Port GPIOA
+#define LED100_Pin GPIO_PIN_5
+#define LED100_GPIO_Port GPIOC
+#define LED101_Pin GPIO_PIN_1
+#define LED101_GPIO_Port GPIOB
+#define LED102_Pin GPIO_PIN_2
+#define LED102_GPIO_Port GPIOB
+#define NC1_Pin GPIO_PIN_15
+#define NC1_GPIO_Port GPIOB
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
--- misko/Core/Src/gpio.c
+++ misko/Core/Src/gpio.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    gpio.c
-  * @brief   This file provides code for the configuration
-  *          of all used GPIO pins.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    gpio.c
+ * @brief   This file provides code for the configuration
+ *          of all used GPIO pins.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -33,27 +33,62 @@
 /* USER CODE END 1 */
 
 /** Configure pins as
-        * Analog
-        * Input
-        * Output
-        * EVENT_OUT
-        * EXTI
-*/
-void MX_GPIO_Init(void)
+ * Analog
+ * Input
+ * Output
+ * EVENT_OUT
+ * EXTI
+ */
+void MX_GPIO_Init (void)
 {
 
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitTypeDef GPIO_InitStruct =
+		{ 0 };
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOF_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin (GPIOC, LED105_Pin | LED100_Pin, GPIO_PIN_SET);
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin (GPIOA, LED107_Pin | LED104_Pin, GPIO_PIN_SET);
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin (GPIOB, LED101_Pin | LED102_Pin, GPIO_PIN_SET);
+
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin (NC1_GPIO_Port, NC1_Pin, GPIO_PIN_RESET);
+
+	/*Configure GPIO pin : PtPin */
+	GPIO_InitStruct.Pin = PushButton_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init (PushButton_GPIO_Port, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PCPin PCPin */
+	GPIO_InitStruct.Pin = LED105_Pin | LED100_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init (GPIOC, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PAPin PAPin */
+	GPIO_InitStruct.Pin = LED107_Pin | LED104_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init (GPIOA, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PBPin PBPin PBPin */
+	GPIO_InitStruct.Pin = LED101_Pin | LED102_Pin | NC1_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init (GPIOB, &GPIO_InitStruct);
 
 }
 
