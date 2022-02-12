68bfbbcf084fc94e267cdd21f7ba4aba6afe0fb4
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2022-02-12 19:57:28 +0100

    - activated SPI1 in interrupt mode

--- misko/Core/Inc/stm32l1xx_it.h
+++ misko/Core/Inc/stm32l1xx_it.h
@@ -57,6 +57,7 @@ void PendSV_Handler(void);
 void SysTick_Handler(void);
 void TIM10_IRQHandler(void);
 void TIM11_IRQHandler(void);
+void SPI1_IRQHandler(void);
 void USART2_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
