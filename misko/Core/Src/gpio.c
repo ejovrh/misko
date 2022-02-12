5c7d6297c4e23e6fb8e2dbbb8cd51c55ae5ad620
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2022-02-12 18:32:33 +0100

    - removed 32kHz LSE from config since it will not be needed

--- misko/Core/Src/gpio.c
+++ misko/Core/Src/gpio.c
@@ -38,7 +38,6 @@
         * Output
         * EVENT_OUT
         * EXTI
-     PA8   ------> RCC_MCO
 */
 void MX_GPIO_Init(void)
 {
@@ -65,14 +64,6 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  GPIO_InitStruct.Alternate = GPIO_AF0_MCO;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
