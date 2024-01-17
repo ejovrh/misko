24248aae4014e665c4496abba0132e395d2d28f3
ejovrh cinavi <hrvoje.ivanic@gmx.net>
2024-01-17 10:22:29 +0100

    in adxl345.c/.h: - first working version of the adxl345 driver

---
+++ misko/Core/Inc/adxl345/adxl345.c
@@ -0,0 +1,148 @@
+#include "adxl345/adxl345.h"
+
+#if defined(USE_ADXL345)	// if this particular device is active
+
+typedef struct	// adxl345c_t actual
+{
+	adxl345_t public;  // public struct
+
+	uint8_t _irq_src;  //
+	SPI_HandleTypeDef *_hspi;  // HAL SPI instance
+	GPIO_TypeDef *_CS_Port;  // SPI chip select GPIO port
+	uint16_t _CS_Pin;  // SPI chip select GPIO pin
+} __adxl345_t;
+
+static __adxl345_t __ADXL345 __attribute__ ((section (".data")));  // preallocate __ADXL345 object in .data
+
+#define REG_CNT 30	// 64 registers
+#define COMMAND_READ 0x80	// SPI read command, DS. p. 15, figure 37
+
+static const uint8_t _RegisterAddress[REG_CNT] =  // address of each register address, indexed by adxl345_reg_t
+	{  //
+	0x00,  // DEVID
+	// mind the address gap for bulk reads!
+	0x1D,// THRESH_TAP
+	0x1E,  // OFSX
+	0x1F,  // OFSY
+	0x20,  // OFSZ
+	0x21,  // DUR
+	0x22,  // LATENT
+	0x23,  // WINDOW
+	0x24,  // THRESH_ACT
+	0x25,  // THRESH_INACT
+	0x26,  // TIME_INACT
+	0x27,  // ACT_INACT_CTL
+	0x28,  // THRESH_FF
+	0x29,  // TIME_FF
+	0x2A,  // TAP_AXES
+	0x2B,  // ACT_TAP_STATUS
+	0x2C,  // BW_RATE
+	0x2D,  // POWER_CTL
+	0x2E,  // INT_ENABLE
+	0x2F,  // INT_MAP
+	0x30,  // INT_SOURCE
+	0x31,  // DATA_FORMAT
+	0x32,  // DATAX0
+	0x33,  // DATAX1
+	0x34,  // DATAY0
+	0x35,  // DATAY1
+	0x36,  // DATAZ0
+	0x37,  // DATAZ1
+	0x38,  // FIFO_CTL
+	0x39,  // FIFO_STATUS
+	};
+
+// reads a bytes from device
+static uint8_t _ReadByte(const adxl345_reg_t in_register)
+{
+	__disable_irq();  // atomic start
+	uint8_t address = (_RegisterAddress[in_register] | COMMAND_READ);  // mark address as read command
+
+	HAL_GPIO_WritePin(__ADXL345._CS_Port, __ADXL345._CS_Pin, GPIO_PIN_RESET);  // select the slave
+
+	HAL_SPI_Transmit(__ADXL345._hspi, &address, 1, 100);  // send register address to read from
+	HAL_SPI_Receive(__ADXL345._hspi, &address, 1, 100);  // send register address to read from
+
+	HAL_GPIO_WritePin(__ADXL345._CS_Port, __ADXL345._CS_Pin, GPIO_PIN_SET);  // de-select the slave
+	__enable_irq();  // atomic end
+
+	return address;
+}
+
+// writes a bytes to device
+static void _WriteByte(const adxl345_reg_t in_register, const uint8_t data)
+{
+	__disable_irq();  // atomic start
+
+	uint8_t address = _RegisterAddress[in_register];
+
+	HAL_GPIO_WritePin(__ADXL345._CS_Port, __ADXL345._CS_Pin, GPIO_PIN_RESET);  // select the slave
+
+	HAL_SPI_Transmit(__ADXL345._hspi, &address, 1, 100);  // send register address to write to
+	HAL_SPI_Transmit(__ADXL345._hspi, &data, 1, 100);  // send data
+
+	HAL_GPIO_WritePin(__ADXL345._CS_Port, __ADXL345._CS_Pin, GPIO_PIN_SET);  // de-select the slave
+	__enable_irq();  // atomic end
+}
+
+//
+void _ISR(void)  // ISR for the ADXL345 accelerometer
+{
+	__ADXL345._irq_src = _ReadByte(INT_SOURCE);  // read interrupt source & thereby clear pending interrupts
+
+	if(((__ADXL345._irq_src >> 3) & 0x01) == 0x01)  // if the inactivity bit is set
+		{
+			_ReadByte(INT_SOURCE);  // clear any pending interrupts
+			_WriteByte(INT_ENABLE, 0x00);  // disable interrupts
+			_WriteByte(INT_ENABLE, INT_ENABLE_ACT_CFG);  // configure for activity detection
+
+			HAL_GPIO_WritePin(GPS_Red_LED_GPIO_Port, GPS_Red_LED_Pin, GPIO_PIN_SET);  // turn LED off
+		}
+
+	if(((__ADXL345._irq_src >> 4) & 0x01) == 0x01)  // if the activity bit is set
+		{
+			_ReadByte(INT_SOURCE);  // clear any pending interrupts
+			_WriteByte(INT_ENABLE, 0x00);  // disable interrupts
+			_WriteByte(INT_ENABLE, INT_ENABLE_INACT_CFG);  // reconfigure for inact. detection
+			_ReadByte(INT_SOURCE);  // enable interrupts
+
+			HAL_GPIO_WritePin(GPS_Red_LED_GPIO_Port, GPS_Red_LED_Pin, GPIO_PIN_RESET);  // turn LED on
+		}
+}
+
+static __adxl345_t __ADXL345 =  // instantiate adxl345c_t actual and set function pointers
+	{  //
+	.public.ISR = &_ISR,	// device ISR
+	.public.ReadByte = &_ReadByte,	// reads a bytes from device
+	.public.WriteByte = &_WriteByte  // writes a bytes to device
+	};
+
+adxl345_t* adxl345_ctor(SPI_HandleTypeDef *in_hspi, GPIO_TypeDef *_SPI_CS_Port, const uint16_t _SPI_CS_Pin)  //
+{
+	__ADXL345._hspi = in_hspi;  // store SPI object
+	__ADXL345._CS_Port = _SPI_CS_Port;	// store CS port
+	__ADXL345._CS_Pin = _SPI_CS_Pin;  // store CS pin
+	HAL_GPIO_WritePin(__ADXL345._CS_Port, __ADXL345._CS_Pin, GPIO_PIN_SET);  // chip select is active low
+
+// basic accelerometer device initialisation & configuration
+	_ReadByte(INT_SOURCE);	// clear any pending interrupts
+
+	_WriteByte(INT_ENABLE, 0x00);  // disable all interrupts
+	_WriteByte(POWER_CTL, POWER_CTL_CFG_OFF);  // all off
+	_WriteByte(DATA_FORMAT, DATA_FORMAT_CFG);  // 16g range, full resolution, active high interrupt
+	_WriteByte(BW_RATE, BW_RATE_CFG);  // low power, output data rate 6.25Hz
+	_WriteByte(THRESH_ACT, 0x4);	// 62.5mg/LSB
+	_WriteByte(THRESH_INACT, 0x4);	// 62.5mg/LSB
+	_WriteByte(TIME_INACT, 0x02);  // 1s/LSB
+	_WriteByte(ACT_INACT_CTL, ACT_INACT_CTL_CFG);  // all axes in AC mode
+	_WriteByte(INT_ENABLE, INT_ENABLE_INACT_CFG);  // enable interrupts only for inact. detection
+	_WriteByte(INT_MAP, 0x00);	// route all to INT1
+
+	_WriteByte(POWER_CTL, POWER_CTL_CFG);  // power on and start
+
+	return &__ADXL345.public;  // set pointer to ADXL345 public part
+}
+
+adxl345_t *const ADXL345 = &__ADXL345.public;  // set pointer to ADXL345 public part
+
+#endif
--- misko/Core/Inc/adxl345/adxl345.h
+++ misko/Core/Inc/adxl345/adxl345.h
@@ -1,4 +1,68 @@
 #ifndef INC_ADXL345_ADXL345_H_
 #define INC_ADXL345_ADXL345_H_
 
+#include "main.h"
+
+#if defined(USE_ADXL345)	// if this particular device is active
+
+// config items for act./inact. detection
+#define	DATA_FORMAT_CFG	0x0B	// 16g range, full resolution, active high int.
+#define	INT_MAP_CFG	0x00	// route all to INT1
+#define	INT_ENABLE_INACT_CFG	0x08	// enable interrupt only for inact. detection
+#define	INT_ENABLE_ACT_CFG	0x10	// enable interrupts only for act. detection
+//#define	TIME_INACT_CFG	0x02	// LSB meaning: 0x05 (dec 5) => 5s , 0x3C (dec 60) => 60s
+//#define	THRESH_INACT_CFG	0x02	// 62.5mg / LSB
+//#define	THRESH_ACT_CFG	0x02	// 62.5mg / LSB
+#define	ACT_INACT_CTL_CFG	0xFF	// enable activity and inactivity detection on x,y,z using AC
+#define POWER_CTL_CFG_OFF	0x00	// all off
+#define	POWER_CTL_CFG	0x2A	// 2Hz sampling on sleep, in measure mode, link mode, no auto sleep
+#define	BW_RATE_CFG	0x16	// low power, output data rate 6.25Hz
+
+typedef enum adxl345_reg_t  // register name to array index translation
+{
+	  DEVID = 0,  //  fixed device ID code of 0xE5, DS. p. 24
+	  THRESH_TAP = 1,  // threshold	value for tap interrupts, DS. p. 24
+	  OFSX = 2,  // user-set offset adjustments in twos complement format, DS. p. 24
+	  OFSY = 3,  // ditto, DS. p. 24
+	  OFSZ = 4,  // ditto, DS. p. 24
+	  DUR = 5,	// event must be above the THRESH_TAP threshold to qualify as a tap event, DS. p. 24
+	  LATENT = 6,  // wait time from the detection of a tap event to the start of the time window, DS. p. 24
+	  WINDOW = 7,  // time after the expiration of the latency time, DS. p. 24
+	  THRESH_ACT = 8,  // threshold value for detecting activity, DS. p. 24
+	  THRESH_INACT = 9,  // threshold value for detecting inactivity, DS. p. 24
+	  TIME_INACT = 10,  // time that acceleration must be less than THRESH_INACT for inactivity to be declared, DS. p. 24
+	  ACT_INACT_CTL = 11,  // act/inact configuration, DS. p. 24
+	  THRESH_FF = 12,  // threshold value for free-fall detection, DS. p. 25
+	  TIME_FF = 13,  // time that the value of all axes must be less than THRESH_FF to generate a free-fall interrupt, DS. p. 25
+	  TAP_AXES = 14,  // tape axes configuration, DS. p. 25
+	  ACT_TAP_STATUS = 15,  // act/tap status bits, DS. p. 25
+	  BW_RATE = 16,  // bandwidth/low power config, DS. p. 25
+	  POWER_CTL = 17,  // power control config, DS. p. 26
+	  INT_ENABLE = 18,  // interrupt enable configuration, DS. p. 26
+	  INT_MAP = 19,  // interrupt map configuration, DS. p. 27
+	  INT_SOURCE = 20,  // interrupt source configuration, DS. p. 27
+	  DATA_FORMAT = 21,  // data format config for registers below, DS. p. 27
+	  DATAX0 = 22,  // acceleration data for axis, DS. p. 27
+	  DATAX1 = 23,  // ditto
+	  DATAY0 = 24,  // ditto
+	  DATAY1 = 25,  // ditto
+	  DATAZ0 = 26,  // ditto
+	  DATAZ1 = 27,  // ditto
+	  FIFO_CTL = 28,  // FIFO configuration, DS. p. 28
+	  FIFO_STATUS = 29  // FIFO status bits, DS. p. 28
+} adxl345_reg_t;
+
+typedef struct	// struct describing the Accelerometer functionality
+{
+	void (*ISR)(void);	// device ISR
+	uint8_t (*ReadByte)(const adxl345_reg_t in_register);  // reads a bytes from device
+	void (*WriteByte)(const adxl345_reg_t in_register, const uint8_t data);  // writes a bytes to device
+} adxl345_t;
+
+adxl345_t* adxl345_ctor(SPI_HandleTypeDef *in_hspi, GPIO_TypeDef *_SPI_CS_Port, const uint16_t _SPI_CS_Pin);  // the ADXL345 constructor
+
+extern adxl345_t *const ADXL345;  // declare pointer to public struct part
+
+#endif
+
 #endif /* INC_ADXL345_ADXL345_H_ */
